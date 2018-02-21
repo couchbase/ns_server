cd711e75e44bd6dc373628e1c647da23e53d6f63
Artem Stemkovski <astemkov@gmail.com>
2018-02-21 12:16:09 -0800

    sanitize all ets tables by default
    because some of them are unnamed and can contain sensitive stuff
    ex. mru_cache sub tables
    
    Change-Id: I22f14b65acc1e8015e123e86966283e1060b6b4d
    Reviewed-on: http://review.couchbase.org/89851
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Amit Krishnan <krish.amit@gmail.com>
    Tested-by: Artem Stemkovski <artem@couchbase.com>

--- src/diag_handler.erl
+++ src/diag_handler.erl
@@ -333,18 +333,14 @@ sanitize_ets_table(menelaus_ui_auth, _Info, _Content) ->
     ["not printed"];
 sanitize_ets_table(menelaus_ui_auth_by_expiration, _Info, _Content) ->
     ["not printed"];
-sanitize_ets_table(ns_config_ets_dup, _Info, Content) ->
-    ns_config_log:sanitize(Content);
 sanitize_ets_table(menelaus_web_cache, _Info, Content) ->
     ns_cluster:sanitize_node_info(Content);
-sanitize_ets_table(bucket_info_cache_buckets, _Info, Content) ->
-    ns_config_log:sanitize(Content);
 sanitize_ets_table(_, Info, Content) ->
     case proplists:get_value(name, Info) of
         ssl_otp_pem_cache ->
             ["not printed"];
         _ ->
-            ns_config_log:tag_user_data(Content)
+            ns_config_log:sanitize(Content, true)
     end.
 
 grab_all_ets_tables() ->
