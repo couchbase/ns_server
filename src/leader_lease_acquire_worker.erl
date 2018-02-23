8e8ff17004ace6a519c02fa697f69e630443f925
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2018-02-23 22:32:14 +0530

    Rework leader_lease_acquire_worker to use new acquire approach.
    Change-Id: Ie91de00c69cb3a6b764ad80ffaa857252a81664e
    Reviewed-on: http://review.couchbase.org/89978
    Tested-by: Aliaksey Artamonau <aliaksey.artamonau@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Artem Stemkovski <artem@couchbase.com>

--- src/leader_lease_acquire_worker.erl
+++ src/leader_lease_acquire_worker.erl
@@ -27,12 +27,10 @@
                  target :: node(),
                  uuid   :: binary(),
 
-                 rto           :: rto:rto(),
-                 retry_backoff :: backoff:backoff(),
-
-                 have_lease    :: boolean(),
-                 acquire_timer :: misc:timer(acquire),
-                 revoke_timer  :: misc:timer(revoke) }).
+                 have_lease      :: boolean(),
+                 lease_expire_ts :: integer(),
+                 retry_backoff   :: backoff:backoff(),
+                 acquire_timer   :: misc:timer(acquire) }).
 
 spawn_monitor(TargetNode, UUID) ->
     Parent = self(),
@@ -40,12 +38,6 @@ spawn_monitor(TargetNode, UUID) ->
 
 init(Parent, TargetNode, UUID) ->
     process_flag(priority, high),
-
-    Rto = rto:new([{window,         ?get_param(window, 600 * 1000)},
-                   {backoff,        ?get_param(backoff, 2)},
-                   {min_timeout,    ?get_param(min_timeout, 2000)},
-                   {max_timeout,    ?get_param(max_timeout, ?LEASE_TIME)},
-                   {var_multiplier, ?get_param(var_multiplier, 4)}]),
 
     RetryBackoff =
         backoff:new([{initial,    ?get_param(retry_initial, 500)},
@@ -56,12 +48,10 @@ init(Parent, TargetNode, UUID) ->
                    target = TargetNode,
                    uuid   = UUID,
 
-                   rto           = Rto,
                    retry_backoff = RetryBackoff,
 
                    have_lease    = false,
-                   acquire_timer = misc:create_timer(acquire),
-                   revoke_timer  = misc:create_timer(revoke)},
+                   acquire_timer = misc:create_timer(acquire)},
 
     loop(acquire_now(State)).
 
@@ -69,8 +59,6 @@ loop(State) ->
     receive
         acquire ->
             loop(handle_acquire_lease(State));
-        revoke ->
-            loop(handle_revoke_lease(State));
         {Ref, _} when is_reference(Ref) ->
             %% this is late gen_server call response, so just skipping
             loop(State);
@@ -83,28 +71,25 @@ loop(State) ->
 %% To simplify the code elsewhere, we may try to "revoke" the lease when we
 %% actually are not holding it. In such a case, we simply schedule another
 %% attempt to acquire the lease.
-handle_revoke_lease(#state{have_lease = false} = State) ->
+revoke_lease(#state{have_lease = false} = State) ->
     retry_acquire(State);
-handle_revoke_lease(State) ->
+revoke_lease(State) ->
     ok = leader_activities:lease_lost(parent(State), target_node(State)),
     retry_acquire(State#state{have_lease = false}).
 
-handle_acquire_lease(#state{target = TargetNode,
-                            uuid   = UUID} = State) ->
-    Start   = time_compat:monotonic_time(),
-    Timeout = get_acquire_timeout(State),
+handle_acquire_lease(State) ->
+    Start   = time_compat:monotonic_time(millisecond),
+    Options = get_acquire_lease_options(Start, State),
 
-    try leader_lease_agent:acquire_lease(TargetNode, node(), UUID,
-                                         [{period, ?LEASE_TIME},
-                                          {timeout, Timeout}]) of
+    try call_acquire_lease(Options, State) of
         {ok, LeaseProps} ->
             handle_lease_acquired(Start, LeaseProps, State);
         {error, timeout} ->
-            handle_acquire_timeout(Timeout, State);
+            handle_acquire_timeout(Options, State);
         {error, lease_lost} ->
-            handle_lease_lost(Start, State);
+            handle_lease_lost(State);
         {error, {already_acquired, LeaseProps}} ->
-            handle_lease_already_acquired(Start, LeaseProps, State);
+            handle_lease_already_acquired(LeaseProps, State);
         Other ->
             handle_unexpected_response(Other, State)
     catch
@@ -112,14 +97,24 @@ handle_acquire_lease(#state{target = TargetNode,
             handle_exception({T, E}, State)
     end.
 
-handle_acquire_timeout(Timeout, State) ->
+get_acquire_lease_options(Start, State) ->
+    Timeout       = get_acquire_timeout(Start, State),
+    WhenRemaining = 2 * ?LEASE_TIME div 3,
+
+    [{period, ?LEASE_TIME},
+     {timeout, Timeout},
+     {when_remaining, WhenRemaining}].
+
+call_acquire_lease(Options, #state{uuid = UUID, target = TargetNode}) ->
+    leader_lease_agent:acquire_lease(TargetNode, node(), UUID, Options).
+
+handle_acquire_timeout(AcquireOptions, State) ->
     ?log_warning("Timeout while trying to acquire lease from ~p. "
-                 "Timeout was ~bms", [target_node(State), Timeout]),
+                 "Acquire options where ~bms",
+                 [target_node(State), AcquireOptions]),
 
     functools:chain(State,
-                    [backoff_rto(_),
-
-                     %% Since we timed out, it most likely doesn't make sense
+                    [%% Since we timed out, it most likely doesn't make sense
                      %% to wait much before retrying, so we just reset the
                      %% timeout to the lowest value.
                      reset_retry(_),
@@ -127,27 +122,29 @@ handle_acquire_timeout(Timeout, State) ->
                      %% The way we pick the timeout pretty much ensures that
                      %% we don't have time to retry. So we backoff to try to
                      %% avoid this in future and also revoking the lease.
-                     revoke_now(_)]).
+                     revoke_lease(_)]).
 
 handle_lease_acquired(StartTime, LeaseProps, State) ->
-    Now        = time_compat:monotonic_time(),
-    TimePassed = misc:convert_time_unit(Now - StartTime, millisecond),
-
     maybe_notify_lease_acquired(State),
-
-    {_, TimeLeft0} = lists:keyfind(time_left, 1, LeaseProps),
-
-    TimeLeft    = max(0, TimeLeft0 - TimePassed - ?LEASE_GRACE_TIME),
-    ExtendAfter = get_extend_timeout(StartTime, TimeLeft, State),
 
     NewState = State#state{have_lease = true},
     functools:chain(NewState,
-                    [reset_retry(_),
-                     update_rtt(StartTime, _),
-                     acquire_after(ExtendAfter, _),
-                     revoke_after(TimeLeft, _)]).
+                    [update_lease_expire_ts(StartTime, LeaseProps, _),
+                     reset_retry(_),
+                     acquire_now(_)]).
 
-handle_lease_already_acquired(StartTime, LeaseProps, State) ->
+update_lease_expire_ts(Start, LeaseProps, State) ->
+    Now        = time_compat:monotonic_time(millisecond),
+    TimePassed = Now - Start,
+
+    {_, TimeLeft} = lists:keyfind(time_left, 1, LeaseProps),
+    TimeQueued    = proplists:get_value(time_queued, LeaseProps, 0),
+    TimeInFlight  = TimePassed - TimeQueued,
+    ExpiresAfter  = TimeLeft - TimeInFlight - ?LEASE_GRACE_TIME,
+
+    State#state{lease_expire_ts = Now + ExpiresAfter}.
+
+handle_lease_already_acquired(LeaseProps, State) ->
     {node, Node}          = lists:keyfind(node, 1, LeaseProps),
     {uuid, UUID}          = lists:keyfind(uuid, 1, LeaseProps),
     {time_left, TimeLeft} = lists:keyfind(time_left, 1, LeaseProps),
@@ -157,8 +154,7 @@ handle_lease_already_acquired(StartTime, LeaseProps, State) ->
                  [target_node(State), {Node, UUID}, TimeLeft]),
 
     functools:chain(State,
-                    [update_rtt(StartTime, _),
-                     reset_retry(_),
+                    [reset_retry(_),
 
                      %% Being pessimistic here and assuming that our
                      %% communication to the remote node was instantaneous.
@@ -169,69 +165,33 @@ handle_unexpected_response(Resp, State) ->
                  "response ~p from node ~p", [Resp, target_node(State)]),
 
     %% Well, don't have much else to do.
-    revoke_now(backoff_retry(State)).
+    revoke_lease(backoff_retry(State)).
 
 handle_exception(Exception, State) ->
     ?log_warning("Failed to acquire lease from ~p: ~p",
                  [target_node(State), Exception]),
 
-    revoke_now(backoff_retry(State)).
+    revoke_lease(backoff_retry(State)).
 
-handle_lease_lost(StartTime, State) ->
+handle_lease_lost(State) ->
     ?log_warning("Node ~p told us that we lost its lease", [target_node(State)]),
 
     %% this is supposed to be a very short condition, so we don't backoff here
     %% and even update the round trip time
-    revoke_now(update_rtt(StartTime, State)).
+    revoke_lease(State).
 
 %% Since acquire_lease internally is just a gen_server call, we can be sure
 %% that our message either reaches the worker process on target node or we get
 %% an exception as a result of the distribution connection between the nodes
 %% being closed. So we want to pick the timeout as long as possible, but so
 %% that we can process the revocation in a timely way.
-get_acquire_timeout(#state{have_lease = false}) ->
+get_acquire_timeout(_Now, #state{have_lease = false}) ->
     %% Well, if that's not enough, we probably won't be able to keep the lease
     %% anyway.
     ?LEASE_TIME;
-get_acquire_timeout(#state{revoke_timer = RevokeTimer}) ->
-    TimeLeft = misc:read_timer(RevokeTimer),
-    true = is_integer(TimeLeft),
-
-    TimeLeft.
-
-get_extend_timeout(StartTime, TimeLeft, #state{rto = Rto}) ->
-    RtoTimeout = rto:get_timeout(Rto),
-
-    %% Just so that we don't sit in a busy loop.
-    MinExtendTimeout = get_min_extend_timeout(StartTime),
-
-    %% The buffer we leave for ourselves consists of two parts:
-    %%
-    %%   - Retransmission timeout.
-    %%   - The time passed since we started the acquire call.
-    %%
-    %% The former is our idea of how much time we need for acquire to succeed
-    %% at most. The latter is our actual round trip time for this call. So
-    %% it's quite pessimistic (which is good here) since it's not very likely
-    %% that all this time was spent on the way back from the target node.
-    Timeout = TimeLeft - RtoTimeout,
-
-    max(MinExtendTimeout, Timeout).
-
-get_min_extend_timeout(StartTime) ->
-    Now        = time_compat:monotonic_time(),
-    SinceStart = misc:convert_time_unit(Now - StartTime, millisecond),
-
-    max(0, ?get_param(min_extend_timeout, 100) - SinceStart).
-
-update_rtt(Start, State) ->
-    Now  = time_compat:monotonic_time(),
-    Diff = misc:convert_time_unit(Start - Now, millisecond),
-
-    misc:update_field(#state.rto, State, rto:note_success(Diff, _)).
-
-backoff_rto(State) ->
-    misc:update_field(#state.rto, State, rto:note_failure(_)).
+get_acquire_timeout(Now, #state{have_lease      = true,
+                                lease_expire_ts = ExpireTS}) ->
+    max(ExpireTS - Now, 0).
 
 get_retry_timeout(State) ->
     backoff:get_timeout(State#state.retry_backoff).
@@ -247,12 +207,6 @@ acquire_now(State) ->
 
 acquire_after(Timeout, State) ->
     misc:update_field(#state.acquire_timer, State, misc:arm_timer(Timeout, _)).
-
-revoke_now(State) ->
-    revoke_after(0, State).
-
-revoke_after(Timeout, State) ->
-    misc:update_field(#state.revoke_timer, State, misc:arm_timer(Timeout, _)).
 
 target_node(#state{target = Node}) ->
     Node.
