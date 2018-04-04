3ddc44e0a4720cbb830472f8b3e4300b98862055
Pavel Blagodov <stochmail@gmail.com>
2018-04-04 10:57:04 +0300

    MB-29044: filter an empty roles
    Change-Id: I77e459b454524b51020f307555927ed78df97b50
    Reviewed-on: http://review.couchbase.org/92107
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Pavel Blagodov <stochmail@gmail.com>
    Reviewed-by: Pavel Blagodov <stochmail@gmail.com>

--- priv/public/ui/app/mn_admin/mn_security/mn_user_roles/add_dialog/mn_user_roles_add_dialog_controller.js
+++ priv/public/ui/app/mn_admin/mn_security/mn_user_roles/add_dialog/mn_user_roles_add_dialog_controller.js
@@ -18,6 +18,7 @@
     vm.selectedRoles = {};
     vm.getUIID = mnUserRolesService.getRoleUIID;
     vm.toggleWrappers = toggleWrappers;
+    vm.filterEmptyRoles = filterEmptyRoles;
     vm.focusError = false;
 
     activate();
@@ -83,6 +84,10 @@
           reviewSelectedWrappers();
         }
       }
+    }
+
+    function filterEmptyRoles(role) {
+      return !!role;
     }
 
     function activate() {
