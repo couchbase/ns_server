a33a78f77dbf9bab7d984f3b89e17cdcf46aba59
Pavel Blagodov <stochmail@gmail.com>
2018-10-16 11:08:44 +0300

    MB-25475: clean up mnClientCertificate
    Change-Id: Ia11c31a42eaafe8eaf400b46470ecdb4e75ec620
    Reviewed-on: http://review.couchbase.org/100462
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Pavel Blagodov <stochmail@gmail.com>
    Tested-by: Pavel Blagodov <stochmail@gmail.com>

--- priv/public/ui/app-new/mn-client-certificate-component.js
+++ priv/public/ui/app-new/mn-client-certificate-component.js
@@ -14,13 +14,13 @@ mn.components.MnClientCertificate =
     ];
 
     MnClientCertificate.parameters = [
+      mn.services.MnForm,
       mn.services.MnSecurity,
       mn.services.MnPermissions,
       mn.services.MnAlerts,
       mn.services.MnAdmin
     ];
 
-    MnClientCertificate.prototype.maybeDisableField = maybeDisableField;
     MnClientCertificate.prototype.setInitialValue = setInitialValue;
     MnClientCertificate.prototype.getValue = getValue;
     MnClientCertificate.prototype.addItem = addItem;
@@ -28,84 +28,61 @@ mn.components.MnClientCertificate =
 
     return MnClientCertificate;
 
-    function MnClientCertificate(mnSecurityService, mnPermissionsService, mnAlertsService, mnAdminService) {
+    function MnClientCertificate(mnFormService, mnSecurityService, mnPermissionsService, mnAlertsService, mnAdminService) {
       mn.core.MnEventableComponent.call(this);
-
-      this.onSubmit = new Rx.Subject();
-      this.clientCertificateForm = new ng.forms.FormGroup({
-        state: new ng.forms.FormControl(),
-        prefixes: new ng.forms.FormArray([])
-      });
 
       this.securityWrite = mnPermissionsService.createPermissionStream("admin.security!write");
       this.atLeast50 = mnAdminService.stream.compatVersion.pipe(Rx.operators.pluck("atLeast50"));
       this.atLeast51 = mnAdminService.stream.compatVersion.pipe(Rx.operators.pluck("atLeast51"));
-      this.postClientCertAuth = mnSecurityService.stream.postClientCertAuth;
 
-      this.isErrorString = this.postClientCertAuth.error.pipe(
-        Rx.operators.map(function (rv) {
-          return _.isString(rv);
-        })
-      );
+      var getValue = Rx.pipe(Rx.operators.withLatestFrom(this.atLeast50, this.atLeast51),
+                             Rx.operators.map(this.getValue.bind(this)));
 
-      this.isStateDisabled = this.clientCertificateForm.valueChanges.pipe(
-        Rx.operators.pluck("state"),
-        Rx.operators.distinctUntilChanged(),
-        Rx.operators.map(function (value) {
-          return value == "disable";
-        })
-      );
+      this.form = mnFormService.create(this);
 
+      this.form
+        .setFormGroup({state: false, prefixes: this.form.builder.array([])})
+        .submittable(mnSecurityService.stream.postClientCertAuth, getValue)
+        .clearErrors()
+        .message("Settings saved successfully!");
+
+      var initData = mnSecurityService.stream.getClientCertAuth.pipe(Rx.operators.first());
+      var formValues = Rx.merge(this.form.group.valueChanges, initData);
+
+      this.isErrorString =
+        this.form.postRequest.error.pipe(Rx.operators.map(R.is(String)));
+      this.isStateDisabled =
+        formValues.pipe(Rx.operators.map(R.pipe(R.path(["state"]), R.equals("disable"))));
       this.isLastPrefix =
-        this.clientCertificateForm.valueChanges.pipe(
-          Rx.operators.pluck("prefixes", "length"),
-          Rx.operators.map(function (length) {
-            return length === 1;
-          })
-        );
-      mnSecurityService.stream.getClientCertAuth
-        .pipe(Rx.operators.takeUntil(this.mnOnDestroy))
-        .subscribe(this.setInitialValue.bind(this));
+        formValues.pipe(Rx.operators.map(R.pipe(R.path(["prefixes", "length"]), R.equals(1))));
 
       this.securityWrite
         .pipe(Rx.operators.takeUntil(this.mnOnDestroy))
-        .subscribe(this.maybeDisableField.bind(this));
+        .subscribe(R.pipe(R.not, this.form.disableFields("state")));
 
       this.maybeDisableControls =
-        Rx.combineLatest(
-          this.isStateDisabled,
-          this.securityWrite
-        ).pipe(
-          Rx.operators.map(function (values) {
-            return !values[1] || values[0];
-          }),
-          Rx.operators.takeUntil(this.mnOnDestroy)
-        );
+        Rx.combineLatest(this.isStateDisabled, this.securityWrite)
+        .pipe(Rx.operators.map(R.pipe(R.equals([false, true]), R.not)));
 
       this.maybeDisableControls
         .pipe(Rx.operators.takeUntil(this.mnOnDestroy))
-        .subscribe(maybeDisablePrefixesField.bind(this));
+        .subscribe(this.form.disableFields("prefixes"));
 
-      this.postClientCertAuth.success
-        .pipe(Rx.operators.takeUntil(this.mnOnDestroy))
-        .subscribe(mnAlertsService.success("Settings saved successfully!"))
-
-      this.onSubmit.pipe(
-        Rx.operators.tap(this.postClientCertAuth.clearError.bind(this.postClientCertAuth)),
-        Rx.operators.withLatestFrom(this.atLeast50, this.atLeast51),
-        Rx.operators.map(this.getValue.bind(this)),
-        Rx.operators.takeUntil(this.mnOnDestroy)
-      ).subscribe(this.postClientCertAuth.post.bind(this.postClientCertAuth));
+      initData.subscribe(this.setInitialValue.bind(this));
     }
 
     function removeField() {
-      var last = this.clientCertificateForm.get('prefixes').length - 1;
-      this.clientCertificateForm.get('prefixes').removeAt(last);
+      var last = this.form.group.get('prefixes').length - 1;
+      this.form.group.get('prefixes').removeAt(last);
+    }
+
+    function addItem(value) {
+      this.form.group.get("prefixes").push(this.form.builder.group(value));
     }
 
     function getValue(value) {
-      var state =  this.clientCertificateForm.get("state");
-      var prefixes = this.clientCertificateForm.get("prefixes");
+      var state =  this.form.group.get("state");
+      var prefixes = this.form.group.get("prefixes");
       if (!value[1] && value[0]) {
         return [{
           state: state.value,
@@ -121,27 +98,13 @@ mn.components.MnClientCertificate =
       }
     }
 
-    function maybeDisablePrefixesField(value) {
-      this.clientCertificateForm.get("prefixes")[value ? "disable" : "enable"]({onlySelf: true});
-    }
-    function maybeDisableField(value) {
-      this.clientCertificateForm.get("state")[value ? "enable" : "disable"]({onlySelf: true});
-    }
-    function addItem(value) {
-      this.clientCertificateForm.get("prefixes").push(new ng.forms.FormGroup({
-        path: new ng.forms.FormControl(value.path),
-        prefix: new ng.forms.FormControl(value.prefix),
-        delimiter: new ng.forms.FormControl(value.delimiter)
-      }))
-    }
-
     function setInitialValue(value) {
       if (value.prefixes.length) {
         value.prefixes.forEach(this.addItem.bind(this));
       } else {
         this.addItem({delimiter: '', prefix: '', path: 'subject.cn'});
       }
-      this.clientCertificateForm.get("state").patchValue(value.state);
+      this.form.group.get("state").patchValue(value.state);
     }
 
   })(window.rxjs);
