04967a8a29fd771e815d747d5840cd01c8358ad2
Ajit Yagaty <ajit.yagaty@couchbase.com>
2019-02-19 01:41:04 -0800

    MB-32256: Wrapper APIs to handle tcp & ssl...
    ...socket types.
    
    Change-Id: I1d45a93def1fbceba31fbbaf436ced47d568df26
    Reviewed-on: http://review.couchbase.org/102831
    Reviewed-by: Artem Stemkovski <artem@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Ajit Yagaty <ajit.yagaty@couchbase.com>

---
+++ src/network.erl
@@ -0,0 +1,71 @@
+%% @author Couchbase <info@couchbase.com>
+%% @copyright 2019 Couchbase, Inc.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%      http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+
+%% @doc methods/wrappers related to networking.
+
+-module(network).
+
+-export([
+         socket_connect/5,
+         socket_setopts/2,
+         socket_send/2,
+         socket_recv/3,
+         socket_close/1
+        ]).
+
+-spec socket_connect(tcp | ssl,
+                     inet:socket_address() | inet:hostname(),
+                     inet:port_number(),
+                     [gen_tcp:connect_option()],
+                     timeout()) ->
+                            {ok, gen_tcp:socket() | ssl:sslsocket()} |
+                            {error, term()}.
+socket_connect(ssl, Host, Port, Opts, Timeout) ->
+    ssl:connect(Host, Port, Opts, Timeout);
+socket_connect(tcp, Host, Port, Opts, Timeout) ->
+    gen_tcp:connect(Host, Port, Opts, Timeout).
+
+-spec socket_setopts(ssl:sslsocket() | gen_tcp:socket(),
+                     [gen_tcp:option()]) ->
+                            ok | {error, term()}.
+socket_setopts({sslsocket, _, _} = Sock, Opts) ->
+    ssl:setopts(Sock, Opts);
+socket_setopts(Sock, Opts) ->
+    inet:setopts(Sock, Opts).
+
+-spec socket_send(ssl:sslsocket() | gen_tcp:socket(),
+                  term()) ->
+                  ok | {error, term()}.
+socket_send({sslsocket, _, _} = Socket, Data) ->
+    ssl:send(Socket, Data);
+socket_send(Socket, Data) ->
+    prim_inet:send(Socket, Data).
+
+-spec socket_recv(ssl:sslsocket() | gen_tcp:socket(),
+                  integer(), timeout()) ->
+                         {ok, binary() | list()} |
+                         {error, term()}.
+socket_recv({sslsocket, _, _} = Socket, NumBytes, Timeout) ->
+    ssl:recv(Socket, NumBytes, Timeout);
+socket_recv(Socket, NumBytes, Timeout) ->
+    prim_inet:recv(Socket, NumBytes, Timeout).
+
+-spec socket_close(Socket) -> term() when
+      Socket :: ssl:sslsocket() | gen_tcp:socket().
+socket_close({sslsocket, _, _} = Socket) ->
+    ssl:close(Socket);
+socket_close(Socket) ->
+    gen_tcp:close(Socket).
