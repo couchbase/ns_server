0c3470528adf195a35ced1da34f2178f59fae7b2
Pavel Blagodov <stochmail@gmail.com>
2019-04-22 16:27:07 +0300

    MB-33705: fix calculation of total memory quota
    Change-Id: I0927a57f96a63bb0513804dc34f1fbecd66a43b2
    Reviewed-on: http://review.couchbase.org/108118
    Reviewed-by: Pavel Blagodov <stochmail@gmail.com>
    Tested-by: Pavel Blagodov <stochmail@gmail.com>

--- priv/public/ui/app/components/directives/mn_memory_quota/mn_memory_quota.js
+++ priv/public/ui/app/components/directives/mn_memory_quota/mn_memory_quota.js
@@ -30,6 +30,33 @@
       $scope.mnMemoryQuotaController = $scope;
 
       $scope.change = mnMemoryQuotaService.handleAltAndClick;
+
+      $scope.calculateTotalQuota = calculateTotalQuota;
+
+      function getServiceFieldName(service) {
+        switch (service) {
+        case "kv": return "memoryQuota";
+        default: return (service + "MemoryQuota");
+        }
+      }
+
+      function calculateTotalQuota() {
+        return Object
+          .keys($scope.config.services.model)
+          .reduce(function (total, service) {
+            var cfg = $scope.config;
+            var fieldName = getServiceFieldName(service);
+
+            if (cfg.displayedServices[service] &&
+                cfg.services && cfg.services.model[service] &&
+                cfg[fieldName]) {
+              return total + (Number(cfg[fieldName]) || 0);
+            } else {
+              return total;
+            }
+
+          }, 0);
+      }
     }
   }
 })();
