88184e1f2538f2cdbc7ebb2036fd3e176864012f
Eben M Haber <eben@couchbase.com>
2019-05-10 16:05:13 -0700

    Add index statistics to statistics table.
    Change-Id: I7db1c8b66681941d4c78ce99bd829136f8e4793d
    Reviewed-on: http://review.couchbase.org/108993
    Reviewed-by: Eben Haber <eben@couchbase.com>
    Tested-by: Eben Haber <eben@couchbase.com>

--- priv/public/ui/app/mn_admin/mn_indexes/mn_gsi/mn_gsi_controller.js
+++ priv/public/ui/app/mn_admin/mn_indexes/mn_gsi/mn_gsi_controller.js
@@ -14,50 +14,27 @@
     'mnElementCrane',
     'ui.bootstrap',
     'mnPromiseHelper',
-    'mnAlertsService',
-    'mnStatisticsNewService'
+    'mnAlertsService','mnAnalyticsService'
   ]).controller('mnGsiController', mnGsiController);
 
-  function mnGsiController($scope, mnGsiService, mnPoller, mnPermissions) {
+  function mnGsiController($scope, mnGsiService, mnPoller, mnPermissions, mnPoolDefault,mnAnalyticsService) {
     var vm = this;
     vm.focusindexFilter = false;
-    vm.currentBucket = mnPermissions.export.bucketNames['.stats!read'][0];
-    vm.onSelectBucket = getStats;
+    vm.currentBucket = mnPermissions.export.bucketNames['.stats!read'] ? mnPermissions.export.bucketNames['.stats!read'][0] : "";
+    //vm.onSelectBucket = getPageStats;
 
     function activate() {
+      // quckly get just the names of the indexes
+      mnGsiService.getIndexesState(null, false).then(function(res) {vm.state = res;});
+
+      // then poll to get the data at intervals, with index stats
       new mnPoller($scope, function () {
-       return mnGsiService.getIndexesState();
+       return mnGsiService.getIndexesState(null, true); // get them with stats
       })
-      .setInterval(10000)
+      .setInterval(5000)
       .subscribe("state", vm)
       .reloadOnScopeEvent("indexStatusURIChanged")
       .cycle();
-
-      new mnPoller($scope, getStats)
-      .setInterval(5000)
-      .subscribe("stats", vm)
-      .cycle();
-    }
-
-    // for the index display, the following array has the list of stats
-    var stat_names = ["cbas_disk_used","index_memory_quota","index_memory_used","index_ram_percent","index_remaining_ram",
-      "ep_dcp_views+indexes_count","ep_dcp_views+indexes_items_remaining","ep_dcp_views+indexes_producer_count",
-      "ep_dcp_views+indexes_total_backlog_size","ep_dcp_views+indexes_total_bytes","ep_dcp_views+indexes_backoff",
-      "index/fragmentation","index/memory_used","index/disk_size","index/data_size"];
-
-    function getStats() {
-      return mnGsiService.getIndexStats(stat_names,vm.currentBucket).then(function success(resp) {
-        var result = {};
-        if (resp) resp.forEach(function (aStat) {
-          if (aStat.data && aStat.data.statName && aStat.data.stats.aggregate.samples.length > 0) {
-            var sum = 0;
-            for (var i=0; i<aStat.data.stats.aggregate.samples.length; i++)
-              sum += aStat.data.stats.aggregate.samples[i];
-            result[aStat.data.statName] = sum/aStat.data.stats.aggregate.samples.length;
-          }
-        });
-        return result;
-      });
     }
 
     // done with config
