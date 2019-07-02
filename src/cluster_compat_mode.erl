dc2ffce78273b753378ab82ac1949bfc112a69b9
Timofey Barmin <timofey.barmin@couchbase.com>
2019-07-02 13:04:43 -0700

    Revert "MB-33391: Make build Developer Preview by default for beta"
    This reverts commit 8ea5a9764f7a5bdee4f71d899bba739048c8c8e7.
    
    Change-Id: I63dc39ee179cd418b755373c7999a81fe9e5c819
    Reviewed-on: http://review.couchbase.org/111508
    Tested-by: Timofey Barmin <timofey.barmin@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Finlay <dave.finlay@couchbase.com>

--- src/cluster_compat_mode.erl
+++ src/cluster_compat_mode.erl
@@ -315,9 +315,7 @@ get_pretend_version() ->
 
 is_developer_preview() -> is_developer_preview(ns_config:get()).
 is_developer_preview(Config) ->
-    %% The default value should be returned back to false after
-    %% beta concludes
-    ns_config:search(Config, developer_preview_enabled, true).
+    ns_config:search(Config, developer_preview_enabled, false).
 
 tls_supported() ->
     is_enterprise().
