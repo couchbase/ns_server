f8fed5b739ec9f98d253c0029108d2c86063eef4
Timofey Barmin <timofey.barmin@couchbase.com>
2019-07-12 19:32:10 -0700

    MB-34919: Revert "MB-27204 On worker failure in ns_memcached ...
    ... try to reconnect"
    
    This reverts commit 5576c9dc620219dc8c5d014300d9bb99c299a82f.
    
    Change-Id: If2d82308c99c8468cc20b1c0433ae713e2387b9f
    Reviewed-on: http://review.couchbase.org/111944
    Reviewed-by: Dave Finlay <dave.finlay@couchbase.com>
    Tested-by: Timofey Barmin <timofey.barmin@couchbase.com>

--- src/ns_memcached.erl
+++ src/ns_memcached.erl
@@ -176,16 +176,7 @@ run_connect_phase(Parent, Bucket, WorkersCount) ->
 
 worker_init(Parent, ParentState) ->
     ParentState1 = do_worker_init(ParentState),
-    try
-        worker_loop(Parent, ParentState1, #state.running_fast)
-    catch
-        T:E ->
-            Stack = erlang:get_stacktrace(),
-            ?log_error("Unexpected exception in ns_memcached for bucket ~p: ~p",
-                       [ParentState1#state.bucket, {T, E, Stack}]),
-            gen_tcp:close(ParentState1#state.sock),
-            worker_init(Parent, ParentState)
-    end.
+    worker_loop(Parent, ParentState1, #state.running_fast).
 
 do_worker_init(State) ->
     {ok, Sock} = connect([json]),
