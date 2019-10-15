ae1b15b1113c36216934a578f37f1033cf466611
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2019-10-15 11:12:35 -0700

    Include stolen CPU into system stats in the heartbeat.
    Change-Id: Idcdbecb22700913e68df85a7e09f5a0c658fcb3c
    Reviewed-on: http://review.couchbase.org/116454
    Reviewed-by: Artem Stemkovski <artem@couchbase.com>
    Tested-by: Aliaksey Artamonau <aliaksey.artamonau@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>

--- src/ns_heart.erl
+++ src/ns_heart.erl
@@ -358,7 +358,8 @@ current_status_slow_inner() ->
          {node_storage_conf, StorageConf},
          {statistics, erlang_stats()},
          {system_stats, [{N, proplists:get_value(N, SystemStats, 0)}
-                         || N <- [cpu_utilization_rate, swap_total, swap_used,
+                         || N <- [cpu_utilization_rate, cpu_stolen_rate,
+                                  swap_total, swap_used,
                                   mem_total, mem_free, mem_limit,
                                   cpu_cores_available]]},
          {interesting_stats, InterestingStats},
