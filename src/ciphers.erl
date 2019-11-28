57e7c009aa3e96358521a30856367c112f11f57a
Timofey Barmin <timofey.barmin@couchbase.com>
2019-11-28 11:30:35 -0800

    MB-37083: Reorder high ciphers for cbauth
    Two reasons:
    1) More secure ciphers should go first
    2) To work around the following problem in golang servers:
       "The issue is that we negotiate HTTP/2 but the cipher suites list
        provided by cbauth to the query service includes cipher suites
        earlier in the list which are blacklisted
        (see https://http2.github.io/http2-spec/#rfc.section.A). If you
        move those specific cipher suites until later in the list,
        everything begins to work again.  Additionally, it looks like
        this is fixed in later versions of Go (I imagine the server will
        correctly reject the cipher suites known to be blacklisted for
        http2)". See MB-36900 for more info.
    
    Change-Id: I0e44805a82f423e28d5a66b07a229fbf5c9d2a95
    Reviewed-on: http://review.couchbase.org/118648
    Tested-by: Timofey Barmin <timofey.barmin@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Finlay <dave.finlay@couchbase.com>

--- src/ciphers.erl
+++ src/ciphers.erl
@@ -39,12 +39,14 @@ openssl_name(Name) ->
     maps:get(code(Name), openssl_ciphers(), undefined).
 
 high() ->
-    [<<"TLS_RSA_WITH_AES_128_CBC_SHA">>,
-     <<"TLS_RSA_WITH_AES_256_CBC_SHA">>,
+    [<<"TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384">>,
+     <<"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256">>,
+     <<"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384">>,
+     <<"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256">>,
      <<"TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA">>,
      <<"TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA">>,
-     <<"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256">>,
-     <<"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256">>].
+     <<"TLS_RSA_WITH_AES_256_CBC_SHA">>,
+     <<"TLS_RSA_WITH_AES_128_CBC_SHA">>].
 
 medium() ->
     [<<"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA">>,
