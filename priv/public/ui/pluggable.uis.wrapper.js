e095fb37c2639ba361f7768cb3e902f7e577acd0
Pavel Blagodov <stochmail@gmail.com>
2020-03-27 18:15:06 +0000

    Revert "MB-25475: temporarily exclude eventing UI"
    This reverts commit 0cff05d46161fd1ece4c9b86b261a4585e0bdbc9.
    
    Change-Id: I9cfc9588e98dc2db656178edf42a11008002c91a
    Reviewed-on: http://review.couchbase.org/124404
    Reviewed-by: Pavel Blagodov <stochmail@gmail.com>
    Tested-by: Pavel Blagodov <stochmail@gmail.com>

--- priv/public/ui/pluggable.uis.wrapper.js
+++ priv/public/ui/pluggable.uis.wrapper.js
@@ -1,9 +1,11 @@
 import queryUI from "/_p/ui/query/ui-current/query.js";
+import eventingUI from "/_p/ui/event/ui-current/eventing.js";
 import cbasUI from "/_p/ui/cbas/cbas.js";
 import ftsUI from "/_p/ui/fts/fts.js";
 
 export default [
   queryUI,
+  eventingUI,
   cbasUI,
   ftsUI
 ];
