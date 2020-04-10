54254d07f46b45cf8d19879a5440dff3f04d7ed2
Eben M Haber <eben@couchbase.com>
2020-04-10 14:55:17 -0700

    MB-38698 - improve language for 'update notifications'
    The 'update notifications' checkbox and tooltip needed improved
    language to explain what the checkbox actually does, and what data is
    collected as part of the update notification.
    
    Change-Id: If4551bb06eda5c27568623045628dac4d47cec2f
    Reviewed-on: http://review.couchbase.org/c/ns_server/+/125532
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Finlay <dave.finlay@couchbase.com>
    Tested-by: Eben Haber <eben@couchbase.com>

--- priv/public/ui/app/mn_wizard/mn_cluster_configuration/mn_cluster_configuration_controller.js
+++ priv/public/ui/app/mn_wizard/mn_cluster_configuration/mn_cluster_configuration_controller.js
@@ -18,6 +18,7 @@
     vm.onSubmit = onSubmit;
     vm.onIPvChange = onIPvChange;
     vm.sendStats = true;
+    vm.tooltip = 'When this checkbox is enabled, this product automatically collects configuration, usage and performance data, including  cluster information (such as settings and configuration, software version, cluster ID, load levels, and resource quotas), and browser information (such as IP address, inferred geolocation at the city level, and browser type) (collectively with the foregoing, the “Performance Data”). The Performance Data is used by Couchbase to develop and improve our products as well as inform our sales and marketing programs. We do not access or collect any data stored in the Couchbase products. We use this Performance Data to understand aggregate usage patterns and make our products more useful to you. The Performance Data is collected by Couchbase when you access the Admin UI in the configuration wizard if this checkbox is selected. You may turn this feature off at any time from the Admin UI settings page. You can find out more about what data is collected and how it is used if you choose to keep this checkbox enabled <a href="https://docs.couchbase.com/server/current/product-privacy-faq.html">here</a>, which supplements Couchbase’s privacy policy linked <a href="https://www.couchbase.com/privacy-policy">here</a>.';
 
     activate();
 
