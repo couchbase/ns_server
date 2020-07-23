06d1c26a1d1eec1afad974b17eb748ffd85bca4e
Abhijeeth Nuthan <abhijeeth.nuthan@couchbase.com>
2020-07-23 08:55:29 -0700

    MB-38984: Remove couch_log use of error_logger API
    Seems like we ignore events from error_logger anyway.
    
    Change-Id: Ifacaf0519c254f77c18e337c5f2805c2adedc1e6
    Reviewed-on: http://review.couchbase.org/c/ns_server/+/133543
    Tested-by: Abhijeeth Nuthan <abhijeeth.nuthan@couchbase.com>
    Reviewed-by: Timofey Barmin <timofey.barmin@couchbase.com>

--- deps/ns_couchdb/src/couch_log.erl
+++ deps/ns_couchdb/src/couch_log.erl
@@ -14,19 +14,14 @@
 %% limitations under the License.
 
 -module(couch_log).
--behaviour(gen_event).
 
 % public API
--export([start_link/0, stop/0]).
+-export([start_link/0]).
 -export([debug/2, info/2, error/2]).
 -export([debug_on/0, info_on/0, get_level/0, get_level_integer/0, set_level/1]).
 -export([read/2]).
 
 -export([pre_db_open/1]).
-
-% gen_event callbacks
--export([init/1, handle_event/2, terminate/2, code_change/3]).
--export([handle_info/2, handle_call/2]).
 
 -include("ns_common.hrl").
 
@@ -49,13 +44,7 @@ level_integer(debug) -> ?LEVEL_DEBUG;
 level_integer(_Else) -> ?LEVEL_ERROR. % anything else default to ERROR level
 
 start_link() ->
-    couch_event_sup:start_link({local, couch_log}, error_logger, couch_log, []).
-
-stop() ->
-    couch_event_sup:stop(couch_log).
-
-init([]) ->
-    ok.
+    ignore.
 
 debug_on() ->
     true.
@@ -71,21 +60,6 @@ get_level() ->
 
 get_level_integer() ->
     level_integer(get_level()).
-
-handle_event(_Event, State) ->
-    {ok, State}.
-
-handle_call(_Request, State) ->
-    {ok, ok, State}.
-
-handle_info(_Info, State) ->
-    {ok, State}.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-terminate(_Arg, _State) ->
-    ok.
 
 read(_Bytes, _Offset) ->
     {ok, ""}.
