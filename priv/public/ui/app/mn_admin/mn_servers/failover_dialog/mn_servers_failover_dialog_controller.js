92d7bf9536e6dee290db25c2fe7cb654bcc1c220
Pavel Blagodov <stochmail@gmail.com>
2020-08-14 19:23:32 +0200

    MB-40811: show 'eventing' warning only for hard failover
    Change-Id: I044e9f13e9899dec5962b7f8d722a73c894c72b8
    Reviewed-on: http://review.couchbase.org/c/ns_server/+/134381
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Pavel Blagodov <stochmail@gmail.com>
    Reviewed-by: Pavel Blagodov <stochmail@gmail.com>

--- priv/public/ui/app/mn_admin/mn_servers/failover_dialog/mn_servers_failover_dialog_controller.js
+++ priv/public/ui/app/mn_admin/mn_servers/failover_dialog/mn_servers_failover_dialog_controller.js
@@ -43,6 +43,7 @@
     }
     function activate() {
       vm.isEventingNode = node.services.includes("eventing");
+      vm.isKVNode = node.services.includes("kv");
 
       mnPromiseHelper(vm, mnServersService.getNodeStatuses(node.hostname))
         .showSpinner()
