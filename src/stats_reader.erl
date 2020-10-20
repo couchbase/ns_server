fa2a23d564b81dce6902d6da5e3e3cdcbaba740f
Timofey Barmin <timofey.barmin@couchbase.com>
2020-10-20 16:23:53 -0700

    Remove unused code in stats_reader
    Change-Id: Idd0b23c780f4ad9ef9db91a7a3059344d0c1df1b
    Reviewed-on: http://review.couchbase.org/c/ns_server/+/138729
    Tested-by: Timofey Barmin <timofey.barmin@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Sam Cramer <sam.cramer@couchbase.com>

--- src/stats_reader.erl
+++ src/stats_reader.erl
@@ -33,8 +33,7 @@
 -define(TS_TRACKING_TIME_MSEC, 65000).
 
 -record(state, {bucket,
-                last_timestamps,
-                use_new_stats = false}).
+                last_timestamps}).
 
 -export([start_link/1,
          latest/3, latest/4, latest/5,
@@ -112,32 +111,9 @@ code_change(_OldVsn, State, _Extra) ->
 
 init(Bucket) ->
     ns_pubsub:subscribe_link(ns_tick_event),
-    UseNewStats =
-        case Bucket of
-             "@index-" ++ _ -> true;
-             "@xdcr-" ++ _ -> true;
-             "@fts-" ++ _ -> true;
-             "@cbas-" ++ _ -> true;
-             "@eventing-" ++ _ -> true;
-             "@fts" -> true;
-             "@query" -> true;
-             "@index" -> true;
-             "@cbas" -> true;
-             "@eventing" -> true;
-             "@system-processes" -> true;
-             "@system" -> true;
-             "@global" -> true;
-             _ -> true
-        end,
     {ok, #state{bucket=Bucket,
-                last_timestamps = queue:new(),
-                use_new_stats = UseNewStats}}.
+                last_timestamps = queue:new()}}.
 
-
-handle_call({latest, Period}, _From,
-            #state{bucket=Bucket, use_new_stats = false} = State) ->
-    Reply = get_latest_sample(Bucket, Period),
-    {reply, Reply, State};
 
 handle_call({latest, Period}, _From, State) ->
     Res =
@@ -149,27 +125,11 @@ handle_call({latest, Period}, _From, State) ->
         end,
     {reply, Res, State};
 
-handle_call({latest, Period, N}, _From,
-            #state{bucket=Bucket, use_new_stats = false} = State) ->
-    Reply = fetch_latest_sample(Bucket, Period, N),
-    {reply, Reply, State};
-
 handle_call({latest, Period, N}, _From, State) ->
     {reply, get_stats(Period, default_step(Period), N, all, State), State};
 
-handle_call({latest, Period, Step, N}, _From,
-            #state{bucket=Bucket, use_new_stats = false} = State) ->
-    Reply = resample_latest_sample(Bucket, Period, Step, N),
-    {reply, Reply, State};
-
 handle_call({latest, Period, Step, N}, _From, State) ->
     {reply, get_stats(Period, Step, N, all, State), State};
-
-handle_call({latest_specific, Period, StatList}, _From,
-            #state{bucket=Bucket, use_new_stats = false} = State) ->
-    RV = get_latest_sample(Bucket, Period),
-    Reply = extract_stats(StatList, RV),
-    {reply, Reply, State};
 
 handle_call({latest_specific, Period, StatList}, _From, State) ->
     Res =
@@ -181,21 +141,9 @@ handle_call({latest_specific, Period, StatList}, _From, State) ->
         end,
     {reply, Res, State};
 
-handle_call({latest_specific, Period, N, StatList}, _From,
-            #state{bucket=Bucket, use_new_stats = false} = State) ->
-    RV = fetch_latest_sample(Bucket, Period, N),
-    Reply = extract_stats(StatList, RV),
-    {reply, Reply, State};
-
 handle_call({latest_specific, Period, N, StatList}, _From, State) ->
     StatEntries = get_stats(Period, default_step(Period), N, StatList, State),
     {reply, StatEntries, State};
-
-handle_call({latest_specific, Period, Step, N, StatList}, _From,
-            #state{bucket=Bucket, use_new_stats = false} = State) ->
-    RV = resample_latest_sample(Bucket, Period, Step, N),
-    Reply = extract_stats(StatList, RV),
-    {reply, Reply, State};
 
 handle_call({latest_specific, Period, Step, N, StatList}, _From, State) ->
     {reply, get_stats(Period, Step, N, StatList, State), State}.
@@ -232,76 +180,6 @@ multi_node_call(Bucket, Nodes, CallParams) ->
     log_bad_responses(R),
     Replies.
 
-get_latest_sample(Bucket, Period) ->
-    stats_archiver:latest_sample(Bucket, Period).
-
-fetch_latest_sample(Bucket, Period, N) ->
-    try fetch_latest(Bucket, Period, N) of
-        Result -> Result
-    catch Type:Err ->
-            {error, {Type, Err}}
-    end.
-
-resample_latest_sample(Bucket, Period, Step, N) ->
-    try resample(Bucket, Period, Step, N) of
-        Result -> Result
-    catch Type:Err ->
-            {error, {Type, Err}}
-    end.
-
-extract_stats(StatList, {ok, AllStats}) when is_list(AllStats) ->
-    {ok, extract_specific_stats(StatList, AllStats)};
-extract_stats(StatList, {ok, AllStats}) ->
-    [RV] = extract_specific_stats(StatList, [AllStats]),
-    {ok, RV};
-extract_stats(_StatList, Other) ->
-    Other.
-
-%% Extract values for stats specified by the user from AllStats.
-%% AllStats is a list of one or more samples as shown below:
-%%    [{stat_entry, timestamp1,
-%%                  [{stat1,stat1-val},
-%%                   {stat2,stat2-val},
-%%                   {...}|...]},
-%%     {stat_entry, timestamp2,
-%%                  [{stat1,stat1-val},
-%%                   {stat2,stat2-val},
-%%                   {...}|...]},
-%%      ...]
-extract_specific_stats(StatList, AllStats) ->
-    ExtractAllFun = fun (OneSample, AccAll) ->
-                            SV = lists:foldl(
-                                   fun (StatName, Acc) ->
-                                           [{StatName, proplists:get_value(StatName, OneSample#stat_entry.values, undefined)} | Acc]
-                                   end, [], StatList),
-                            [#stat_entry{timestamp = OneSample#stat_entry.timestamp,
-                                         values = SV} | AccAll]
-                    end,
-    lists:reverse(lists:foldl(ExtractAllFun, [], AllStats)).
-
-%% @doc Return the last N records starting with the given key from Tab.
-fetch_latest(Bucket, Period, N) ->
-    case lists:keyfind(Period, 1, stats_archiver:archives()) of
-        false ->
-            {error, bad_period, Period};
-        {_, Interval, _} ->
-            Seconds = N * Interval,
-            Tab = stats_archiver:table(Bucket, Period),
-            case ets:last(Tab) of
-                '$end_of_table' ->
-                    {ok, []};
-                Key ->
-                    Oldest = Key - Seconds * 1000 + 500,
-                    case qlc:eval(qlc:q([Sample || {TS,Sample} <- ets:table(Tab), TS > Oldest])) of
-                        {error, _, _} = Error ->
-                            Error;
-                        Results ->
-                            {ok, Results}
-                    end
-            end
-    end.
-
-
 log_bad_responses({Replies, Zombies}) ->
     case lists:filter(fun ({_, {ok, _}}) -> false; (_) -> true end, Replies) of
         [] ->
@@ -314,39 +192,6 @@ log_bad_responses({Replies, Zombies}) ->
             ok;
         _ ->
             ?stats_error("Some nodes didn't respond: ~p", [Zombies])
-    end.
-
-
-%% @doc Resample the stats in a table. Only reads the necessary number of rows.
-resample(Bucket, Period, Step, N) ->
-    Seconds = N * Step,
-    Tab = stats_archiver:table(Bucket, Period),
-    case ets:last(Tab) of
-        '$end_of_table' ->
-            {ok, []};
-        Key ->
-            Oldest = Key - Seconds * 1000 + 500,
-            Handle = qlc:q([Sample || {TS, Sample}
-                                          <- ets:table(Tab), TS > Oldest]),
-            F = fun (#stat_entry{timestamp = T} = Sample,
-                     {T1, Acc, Chunk}) ->
-                        case misc:trunc_ts(T, Step) of
-                            T1 ->
-                                {T1, Acc, [Sample|Chunk]};
-                            T2 when T1 == undefined ->
-                                {T2, Acc, [Sample]};
-                            T2 ->
-                                {T2, [avg(T1, Chunk)|Acc], [Sample]}
-                        end
-                end,
-            case qlc:fold(F, {undefined, [], []}, Handle) of
-                {error, _, _} = Error ->
-                    Error;
-                {undefined, [], []} ->
-                    {ok, []};
-                {T, Acc, LastChunk} ->
-                    {ok, lists:reverse([avg(T, LastChunk)|Acc])}
-            end
     end.
 
 
