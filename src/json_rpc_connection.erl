d4f269ce88b5980108c96dc96e859615a78f0508
Steve Watanabe <steve.watanabe@couchbase.com>
2021-02-08 11:14:18 -0800

    MB-44177 [BP] Mark user name for redaction in json_rpc log
    This backports the fix tracked via MB-44141:
    
    The entries in the json_rpc.log contain user names that are not marked
    for redaction (surrounded by <ud> and </ud>). This change corrects this.
    
    Change-Id: Ib9d1c7c35fc809386954fca31119c1831b95d908
    Reviewed-on: http://review.couchbase.org/c/ns_server/+/145449
    Reviewed-by: Abhijeeth Nuthan <abhijeeth.nuthan@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Steve Watanabe <steve.watanabe@couchbase.com>

--- src/json_rpc_connection.erl
+++ src/json_rpc_connection.erl
@@ -1,5 +1,5 @@
 %% @author Couchbase <info@couchbase.com>
-%% @copyright 2014-2018 Couchbase, Inc.
+%% @copyright 2014-2021 Couchbase, Inc.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -156,7 +156,7 @@ handle_call({call, Name, EJsonArgThunk}, From, #state{counter = Counter,
               {method, NameB}
               | MaybeParams]},
     ale:debug(?JSON_RPC_LOGGER,
-              "sending jsonrpc call:~p", [ns_config_log:sanitize(EJSON)]),
+              "sending jsonrpc call:~p", [ns_config_log:sanitize(EJSON, true)]),
     ok = gen_tcp:send(Sock, [ejson:encode(EJSON) | <<"\n">>]),
     ets:insert(IdToCaller, {Counter, From}),
     {noreply, State#state{counter = Counter + 1}}.
