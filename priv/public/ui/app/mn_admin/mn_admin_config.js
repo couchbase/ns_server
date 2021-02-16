7652e1140373b9779f5e311e50c204aa5cc33932
Pavel Blagodov <stochmail@gmail.com>
2021-02-16 22:48:00 +0000

    MB-44317: check response status before logout
    Change-Id: I1fb38e147a6eb0246f91338f164f2d9a3d0f2517
    Reviewed-on: http://review.couchbase.org/c/ns_server/+/146251
    Tested-by: Pavel Blagodov <stochmail@gmail.com>
    Reviewed-by: Pavel Blagodov <stochmail@gmail.com>

--- priv/public/ui/app/mn_admin/mn_admin_config.js
+++ priv/public/ui/app/mn_admin/mn_admin_config.js
@@ -83,7 +83,8 @@ function mnAdminConfig($stateProvider, $urlMatcherFactoryProvider, mnPluggableUi
   function interceptorOf401($q, $injector) {
     return {
       responseError: function (rejection) {
-        if (rejection.config.url == "/pools/default" &&
+        if (rejection.status == 401 &&
+            rejection.config.url == "/pools/default" &&
             rejection.config.params && rejection.config.params.etag) {
           $injector.get('mnAuthService').logout();
         }
