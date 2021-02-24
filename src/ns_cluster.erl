e72055b118f5dee1db245fb2c5033828ffe512f5
Artem Stemkovski <astemkov@gmail.com>
2021-02-24 12:49:48 -0800

    remove wait for ejected leader shutdown
    Change-Id: I82e2c4ca53e3427708ac8328bcae5ff1f922f36c
    Reviewed-on: http://review.couchbase.org/c/ns_server/+/147122
    Tested-by: Artem Stemkovski <artem@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Aliaksey Artamonau <aliaksey.artamonau@couchbase.com>

--- src/ns_cluster.erl
+++ src/ns_cluster.erl
@@ -35,7 +35,6 @@
 -define(PREP_CHRONICLE_TIMEOUT, ?get_timeout(prep_chronicle, 240000)).
 -define(JOIN_CHRONICLE_TIMEOUT, ?get_timeout(join_chronicle, 240000)).
 -define(CHANGE_ADDRESS_TIMEOUT, ?get_timeout(change_address, 30000)).
--define(LEADER_SHUTDOWN_TIMEOUT,?get_timeout(leader_shutdown, 30000)).
 
 -define(cluster_log(Code, Fmt, Args),
         ale:xlog(?USER_LOGGER, ns_log_sink:get_loglevel(?MODULE, Code),
@@ -627,8 +626,6 @@ shun(RemoteNode) ->
                 ns_config_rep:ensure_config_pushed(),
                 case chronicle_compat:enabled() of
                     true ->
-                        ok = mb_master:wait_for_ejected_leader_shutdown(
-                               ?LEADER_SHUTDOWN_TIMEOUT),
                         ok = chronicle_master:remove_peer(RemoteNode);
                     false ->
                         ok
