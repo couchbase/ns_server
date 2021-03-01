2d2ce2a8bf098086388f84fe6caccd2ba88b7778
Raluca Lupu <raluca.lupu@couchbase.com>
2021-03-01 09:21:00 +0000

    MB-43650: Changes for collection TTL input:
    Updated text for info tooltip.
    TTL field is hidden in Community Edition.
    
    Change-Id: I30e4d2cba4afe9ad258819e8c85a7ce23e3929ee
    Reviewed-on: http://review.couchbase.org/c/ns_server/+/147326
    Tested-by: Raluca Lupu <raluca.lupu@couchbase.com>
    Tested-by: Pavel Blagodov <stochmail@gmail.com>
    Reviewed-by: Pavel Blagodov <stochmail@gmail.com>
    Well-Formed: Build Bot <build@couchbase.com>

--- priv/public/ui/app/mn.collections.add.item.component.js
+++ priv/public/ui/app/mn.collections.add.item.component.js
@@ -1,11 +1,12 @@
 import {Component, ChangeDetectionStrategy} from '/ui/web_modules/@angular/core.js';
 import {NgbActiveModal} from '/ui/web_modules/@ng-bootstrap/ng-bootstrap.js';
 import {MnLifeCycleHooksToStream} from './mn.core.js';
-import {takeUntil} from '/ui/web_modules/rxjs/operators.js';
-import {map} from "/ui/web_modules/rxjs/operators.js";
+import {withLatestFrom, map} from '/ui/web_modules/rxjs/operators.js';
+import {pipe} from '/ui/web_modules/rxjs.js';
 
 import {MnFormService} from "./mn.form.service.js";
 import {MnCollectionsService} from './mn.collections.service.js';
+import {MnPoolsService} from './mn.pools.service.js';
 
 export {MnCollectionsAddItemComponent}
 
@@ -20,23 +21,36 @@ class MnCollectionsAddItemComponent extends MnLifeCycleHooksToStream {
   static get parameters() { return [
     NgbActiveModal,
     MnCollectionsService,
-    MnFormService
+    MnFormService,
+    MnPoolsService
   ]}
 
-  constructor(activeModal, mnCollectionsService, mnFormService) {
+  constructor(activeModal, mnCollectionsService, mnFormService, mnPoolsService) {
     super();
     this.activeModal = activeModal;
     this.form = mnFormService.create(this);
     this.addCollectionHttp = mnCollectionsService.stream.addCollectionHttp;
+    this.isEnterprise = mnPoolsService.stream.isEnterprise;
 
     this.form
       .setFormGroup({name: "", maxTTL: 0})
-      .setPackPipe(map(() => [this.bucketName, this.scopeName, this.form.group.value.name, this.form.group.value.maxTTL]))
+      .setPackPipe(pipe(
+        withLatestFrom(this.isEnterprise),
+        map(this.prepareDataForSending.bind(this))))
       .setPostRequest(this.addCollectionHttp)
       .showGlobalSpinner()
       .success(() => {
         mnCollectionsService.stream.updateManifest.next();
         activeModal.close()
       });
+  }
+
+  prepareDataForSending([_, isEnterprise]) {
+    let dataForSending = [this.bucketName, this.scopeName, this.form.group.value.name];
+    if (isEnterprise) {
+      dataForSending.push(this.form.group.value.maxTTL);
+    }
+
+    return dataForSending;
   }
 }
