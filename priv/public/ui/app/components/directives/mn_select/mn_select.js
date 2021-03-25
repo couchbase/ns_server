3e50d97b7019985e076369225babdd7d5930763e
Raluca Lupu <raluca.lupu@couchbase.com>
2021-03-25 20:28:55 +0000

    Add search and keyboard navigation for mn-select.
    Change-Id: I6a30c32b159d044cc5a1ccb19dc0069e6a16f610
    Reviewed-on: http://review.couchbase.org/c/ns_server/+/149664
    Tested-by: Raluca Lupu <raluca.lupu@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Pavel Blagodov <stochmail@gmail.com>

--- priv/public/ui/app/components/directives/mn_select/mn_select.js
+++ priv/public/ui/app/components/directives/mn_select/mn_select.js
@@ -1,9 +1,13 @@
 import angular from "/ui/web_modules/angular.js";
 
+import mnSearch from "/ui/app/components/directives/mn_search/mn_search_directive.js";
+
 export default "mnSelect";
 
 angular
-  .module("mnSelect", [])
+  .module("mnSelect", [
+    mnSearch
+  ])
   .directive("mnSelect", mnSelectDirective);
 
 function mnSelectDirective() {
@@ -16,10 +20,11 @@ function mnSelectDirective() {
       ngModel: "=",
       onSelect: "&",
       mnDisabled: "=",
-      filter: "=?",
+      valuesMapping: "=?",
       capitalize: "=",
       openOnTop: "=",
-      mnHorizontalAlign: "=?"
+      mnHorizontalAlign: "=?",
+      hasSearch: "=?"
     },
     templateUrl: "app/components/directives/mn_select/mn_select.html",
     link: mnSelectController
@@ -30,19 +35,25 @@ function mnSelectDirective() {
   function mnSelectController($scope, $element, $attrs, ngModel) {
     var vm = $scope;
 
-    vm.filter = $attrs['filter'] ? vm.filter : (option) => defaultFilter(option);
+    vm.mnSearch = {value: ""};
+
+    var searchMinimumOptionsNumber = 10;
+    vm.valuesMapping = $attrs['valuesMapping'] ? vm.valuesMapping : (option) => defaultValuesMapping(option);
     vm.mnHorizontalAlign = vm.mnHorizontalAlign || 'left';
     vm.isOpened = false;
     vm.optionClicked = optionClicked;
+    vm.clickSearch = clickSearch;
+    vm.getPreparedValues = getPreparedValues;
+    vm.hasSearchInput = hasSearchInput;
 
     /**
-     * Default filter:
+     * Default values mapping:
      * * if capitalize input flag is true - capitalize the displayed option if it is a string
      * * else leave the option as it is
      * @param option
      * @returns {string}
      */
-    function defaultFilter(option) {
+    function defaultValuesMapping(option) {
       if (vm.capitalize && angular.isString(option) && option) {
         return option[0].toUpperCase() + option.slice(1);
       }
@@ -50,10 +61,27 @@ function mnSelectDirective() {
       return option;
     }
 
+    function getPreparedValues() {
+      vm.preparedValues = vm.labels ? vm.labels : vm.values.map(vm.valuesMapping);
+      return vm.preparedValues;
+    }
+
     function optionClicked(value) {
       vm.onSelect && vm.onSelect({selectedOption: value});
       ngModel.$setViewValue(value);
       vm.isOpened = !vm.isOpened;
+
+      if (vm.hasSearchInput()) {
+        vm.mnSearch.value = '';
+      }
+    }
+
+    function clickSearch(event) {
+      event.stopPropagation();
+    }
+
+    function hasSearchInput() {
+      return (vm.hasSearch && vm.values.length >= searchMinimumOptionsNumber) || false;
     }
   }
 }
