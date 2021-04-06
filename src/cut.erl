7a1ffdb61f6ffe291c6a79005916ce65c25f64de
Steve Watanabe <steve.watanabe@couchbase.com>
2021-04-06 10:37:10 -0700

    CBD-3928 License organization
    This is part of the effort to ensure all files used by Couchbase
    contain correct licensing information.
    
    Change-Id: I552c077b334bae3dacdd792021a6ba88142fd4dd
    Reviewed-on: http://review.couchbase.org/c/ns_server/+/151070
    Tested-by: Steve Watanabe <steve.watanabe@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Finlay <dave.finlay@couchbase.com>

--- src/cut.erl
+++ src/cut.erl
@@ -6,21 +6,8 @@
 %% All modifications are (C) 2011-2013 VMware, Inc.,
 %%                                2017 Couchbase, Inc.
 
-%%
-%% ``The contents of this file are subject to the Erlang Public License,
-%% Version 1.1, (the "License"); you may not use this file except in
-%% compliance with the License. You should have received a copy of the
-%% Erlang Public License along with this software. If not, it can be
-%% retrieved via the world wide web at http://www.erlang.org/.
-%%
-%% Software distributed under the License is distributed on an "AS IS"
-%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
-%% the License for the specific language governing rights and limitations
-%% under the License.
-%%
-%% The Initial Developer of the Original Code is Ericsson Utvecklings AB.
-%% Portions created by Ericsson are Copyright 1999, Ericsson Utvecklings
-%% AB. All Rights Reserved.''
+%% Use of this software is governed by the Erlang Public License,
+%% Version 1.1 included in the file licenses/EPL-1-1.txt.
 %%
 
 -module(cut).
