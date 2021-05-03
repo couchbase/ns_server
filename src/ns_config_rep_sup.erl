1a2ff3acf5dd97c80062fd9b74cf8b76e857974b
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2021-05-03 15:56:26 -0700

    Terminate ns_config_rep_merger if ns_config_rep dies.
    There's no point in keeping ns_config_rep_merger alive when
    ns_config_rep dies. If anything, the former may continue merging
    changes that it might have in the mail box, but upon restart
    ns_config_rep will resync with other nodes anyway. So the work that
    ns_config_rep_merger kept doing is redundant.
    
    Change-Id: I0922491afce8fd21439c2ec2aa649ec4096e446d
    Reviewed-on: http://review.couchbase.org/c/ns_server/+/153071
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Aliaksey Artamonau <aliaksey.artamonau@couchbase.com>
    Reviewed-by: Artem Stemkovski <artem@couchbase.com>
    Reviewed-by: Dave Finlay <dave.finlay@couchbase.com>

--- src/ns_config_rep_sup.erl
+++ src/ns_config_rep_sup.erl
@@ -7,37 +7,29 @@
 %% software will be governed by the Apache License, Version 2.0, included in
 %% the file licenses/APL2.txt.
 %%
--module(ns_node_disco_sup).
+-module(ns_config_rep_sup).
 
 -behavior(supervisor).
 
 -export([start_link/0]).
-
 -export([init/1]).
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-    {ok, {{rest_for_one,
+    {ok, {{one_for_all,
            misc:get_env_default(max_r, 3),
            misc:get_env_default(max_t, 10)},
           get_child_specs()}}.
 
 get_child_specs() ->
     [
-     % gen_event for the node disco events.
-     {ns_node_disco_events,
-      {gen_event, start_link, [{local, ns_node_disco_events}]},
-      permanent, 1000, worker, []},
-     % manages node discovery and health.
-     {ns_node_disco,
-      {ns_node_disco, start_link, []},
-      permanent, 1000, worker, []},
-     % logs node disco events for debugging.
-     {ns_node_disco_log,
-      {ns_node_disco_log, start_link, []},
-      permanent, 1000, worker, []},
-     {ns_config_rep_sup, {ns_config_rep_sup, start_link, []},
-      permanent, infinity, supervisor, []}
+     % merges incoming config changes.
+     {ns_config_rep_merger, {ns_config_rep, start_link_merger, []},
+      permanent, brutal_kill, worker, [ns_config_rep]},
+     % replicates config across nodes.
+     {ns_config_rep, {ns_config_rep, start_link, []},
+      permanent, 1000, worker,
+      [ns_config_rep]}
     ].
