359e5bcea1d774d9138bb5580c222ef4b0354e44
Bryan McCoid <bryan.mccoid@couchbase.com>
2021-05-04 15:37:46 -0700

    MB-46043: Fix disk capacity checking
    Ignore failures to read filesystem data from 'df' tool, and time-bound the calls
    to the port making the shell command. It is still somewhat unknown why the calls
    fail in the first place and will be handled separately.
    
    Change-Id: I3bb0197de4839a001b80c4b44b7b899887c62076
    Reviewed-on: http://review.couchbase.org/c/ns_server/+/152827
    Tested-by: Bryan McCoid <bryan.mccoid@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Finlay <dave.finlay@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>

--- src/menelaus_web_alerts_srv.erl
+++ src/menelaus_web_alerts_srv.erl
@@ -16,6 +16,7 @@
 
 -include("ns_common.hrl").
 -include("ns_stats.hrl").
+-include("cut.hrl").
 
 %% needed to mock ns_config in tests
 -include("ns_config.hrl").
@@ -291,22 +292,20 @@ check(ip, Opaque, _History, _Stats) ->
 
 %% @doc check the capacity of the drives used for db and log files
 check(disk, Opaque, _History, _Stats) ->
-
     Mounts = ns_disksup:get_disk_data(),
-
     UsedPre = [ns_storage_conf:this_node_dbdir(),
                ns_storage_conf:this_node_ixdir(),
                ns_storage_conf:this_node_logdir(),
                ns_audit_cfg:get_log_path()],
-    UsedFiles = [X || {ok, X} <- UsedPre],
-
-    RealPaths = [misc:realpath(File, "/") || File <- UsedFiles],
-
-    UsedMountsTmp =
-        [begin {ok, Mnt} = ns_storage_conf:extract_disk_stats_for_path(Mounts, RealFile),
-               Mnt
-         end || {ok, RealFile} <- RealPaths],
-
+    ExtractStats = ns_storage_conf:extract_disk_stats_for_path(Mounts, _),
+    UsedMountsTmp = [X || {ok, X} <- [begin
+                                          case misc:realpath(Mnt, "/") of
+                                              {ok, RealFile} ->
+                                                  ExtractStats(RealFile);
+                                              _ ->
+                                                  none
+                                          end
+                                      end || {ok, Mnt} <- UsedPre]],
     UsedMounts = sets:to_list(sets:from_list(UsedMountsTmp)),
     {value, Config} = ns_config:search(alert_limits),
     MaxDiskUsed = proplists:get_value(max_disk_used, Config),
@@ -462,7 +461,6 @@ hit_rate_limit(Key, Dict) ->
 
             TimePassed < ?DISK_USAGE_TIMEOUT
     end.
-
 
 %% @doc calculate percentage of overhead and if it is over threshold
 -spec over_threshold(integer(), integer()) -> false | {true, float()}.
