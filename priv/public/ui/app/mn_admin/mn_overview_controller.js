fb64df351206c2d312d97ed9e5764babb114cb9d
Matthew Dawber <matthew.dawber@couchbase.com>
2021-08-19 16:56:15 +0100

    MB-48039 Add '-only' flag to ip family in overview text.
    Add specifier to ip family in the overview text on the heading bar if
    addressFamilyOnly is true.
    
    Change-Id: I26fbf10cfdbb3ea4f80fc4c36d20393a69a49653
    Reviewed-on: http://review.couchbase.org/c/ns_server/+/159709
    Tested-by: Matthew <matthew.dawber@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Pavel Blagodov <stochmail@gmail.com>

--- priv/public/ui/app/mn_admin/mn_overview_controller.js
+++ priv/public/ui/app/mn_admin/mn_overview_controller.js
@@ -121,6 +121,7 @@ function mnOverviewController($scope, $rootScope, mnBucketsService, mnServersSer
 
   vm.getEndings = mnHelper.getEndings;
   vm.addressFamily = mnPoolDefault.export.thisNode.addressFamily;
+  vm.addressFamilyOnly = mnPoolDefault.export.thisNode.addressFamilyOnly;
   vm.nodeEncryption = mnPoolDefault.export.thisNode.nodeEncryption;
 
   activate();
