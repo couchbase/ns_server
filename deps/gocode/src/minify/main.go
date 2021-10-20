620b30023d6554bb7f44b094ad9d934c4145094f
Pavel Blagodov <stochmail@gmail.com>
2021-10-20 15:11:10 +0100

    MB-48738: add initial esbuild script
    Change-Id: I5bd90857f53ed6253d2608dd5e60ba5c76322530
    Reviewed-on: http://review.couchbase.org/c/ns_server/+/163992
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Pavel Blagodov <stochmail@gmail.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

---
+++ deps/gocode/src/minify/main.go
@@ -0,0 +1,70 @@
+// @author Couchbase <info@couchbase.com>
+// @copyright 2016-Present Couchbase, Inc.
+//
+// Use of this software is governed by the Business Source License included
+// in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+// in that file, in accordance with the Business Source License, use of this
+// software will be governed by the Apache License, Version 2.0, included in
+// the file licenses/APL2.txt.
+package main
+
+import (
+	"flag"
+	"os"
+	"log"
+	"github.com/evanw/esbuild/pkg/api"
+)
+
+func main() {
+	inDir := flag.String("in-dir", "", "path to js source dir (required)")
+	outDir := flag.String("out-dir", "", "path to js output dir (required)")
+	flag.Parse()
+	log.SetFlags(0)
+
+	if *inDir == "" {
+		log.Printf("Error: path to js source dir must be specified\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if *outDir == "" {
+		log.Printf("Error: path to js source dir must be specified\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	result := api.Build(api.BuildOptions{
+		// MinifyWhitespace: true,
+		// // MinifyIdentifiers: true,
+		// MinifySyntax: true,
+		KeepNames: true,
+
+		AbsWorkingDir: *inDir + "/app",
+		Outdir: *outDir,
+
+		NodePaths: []string{
+			*inDir + "/web_modules",
+			*inDir + "/libs",
+			*inDir + "/app",
+		},
+		EntryPoints: []string{
+			"main.js",
+		},
+		Bundle: true,
+		Splitting: true,
+		Write: true,
+		Metafile: true,
+		Format: api.FormatESModule,
+		LogLevel: api.LogLevelInfo,
+		Engines: []api.Engine{
+			{api.EngineChrome, "93"},
+			{api.EngineFirefox, "92"},
+			{api.EngineSafari, "14"},
+			{api.EngineEdge, "93"},
+		},
+	})
+
+	if len(result.Errors) > 0 {
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/couchbase/ns_server
+
+go 1.17
+
+require github.com/evanw/esbuild v0.12.27
