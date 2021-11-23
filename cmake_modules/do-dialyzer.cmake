892d34be637f2012fc067622f08da36916a52d5c
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2021-11-23 12:26:36 +0300

    Avoid duplication in do-dialyzer.
    CMake finally supports being able to echo commands before running
    them. This requires cmake 3.15. We do require it in some other
    components already, so there shouldn't be any negative effects.
    
    Change-Id: I98ea1306fdba9fcc5497830a3e838b301ef70bce
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/166212
    Tested-by: Aliaksey Artamonau <aliaksey.artamonau@couchbase.com>
    Reviewed-by: Timofey Barmin <timofey.barmin@couchbase.com>

--- cmake_modules/do-dialyzer.cmake
+++ cmake_modules/do-dialyzer.cmake
@@ -3,25 +3,9 @@ GET_FILENAME_COMPONENT (_couchdb_bin_dir "${COUCHDB_BIN_DIR}" REALPATH)
 
 IF (NOT EXISTS "${COUCHBASE_PLT}")
   MESSAGE ("Generating ${COUCHBASE_PLT}...")
-  EXECUTE_PROCESS (COMMAND "${CMAKE_COMMAND}" -E echo
-    "${DIALYZER_EXECUTABLE}" --output_plt "${COUCHBASE_PLT}" --build_plt
-    --apps compiler crypto erts inets kernel os_mon sasl ssl stdlib xmerl eldap
-           public_key
-    ${_couchdb_bin_dir}/src/couchdb
-    ${_couchdb_bin_dir}/src/couch_set_view
-    ${_couchdb_bin_dir}/src/couch_view_parser
-    ${_couchdb_bin_dir}/src/couch_index_merger
-    ${_couchdb_bin_dir}/src/mapreduce
-    ${_couchdb_bin_dir}/src/mochiweb
-    ${_couchdb_bin_dir}/src/snappy
-    ${_couchdb_bin_dir}/src/etap
-    ${_couchdb_bin_dir}/src/lhttpc
-    ${_couchdb_bin_dir}/src/erlang-oauth
-    ${_couchdb_bin_dir}/src/ejson
-    deps/gen_smtp
-    deps/chronicle)
-
-  EXECUTE_PROCESS (COMMAND "${DIALYZER_EXECUTABLE}" --output_plt "${COUCHBASE_PLT}" --build_plt
+  EXECUTE_PROCESS (
+    COMMAND_ECHO STDOUT
+    COMMAND "${DIALYZER_EXECUTABLE}" --output_plt "${COUCHBASE_PLT}" --build_plt
     --apps compiler crypto erts inets kernel os_mon sasl ssl stdlib xmerl eldap
            public_key
     ${_couchdb_bin_dir}/src/couchdb
@@ -46,15 +30,8 @@ STRING (REGEX REPLACE "ebin/(couch_api_wrap(_httpc)?).beam\;?" "" beamfiles "${b
 FILE (GLOB couchdb_beamfiles RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" deps/ns_couchdb/ebin/*.beam)
 STRING (REGEX REPLACE "deps/ns_couchdb/ebin/couch_log.beam\;?" "" couchdb_beamfiles "${couchdb_beamfiles}")
 
-# If you update the dialyzer command, please also update this echo
-# command so it displays what is invoked. Yes, this is annoying.
-EXECUTE_PROCESS (COMMAND "${CMAKE_COMMAND}" -E echo
-  "${DIALYZER_EXECUTABLE}" --plt "${COUCHBASE_PLT}" ${DIALYZER_FLAGS}
-  --apps ${beamfiles}
-  deps/ale/ebin
-  deps/ns_babysitter/ebin
-  ${couchdb_beamfiles})
 EXECUTE_PROCESS (RESULT_VARIABLE _failure
+  COMMAND_ECHO STDOUT
   COMMAND "${DIALYZER_EXECUTABLE}" --plt "${COUCHBASE_PLT}" ${DIALYZER_FLAGS}
   --apps ${beamfiles}
   deps/ale/ebin
