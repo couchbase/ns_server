3dcc4bf8e113ad2577cd60ac2cc0828f38550443
hareen-kancharla <hareen.kancharla@couchbase.com>
2022-01-14 12:31:40 -0800

    MB-50434: Add password_policy_changed event log only on actual change
    Change-Id: I49ce7aed8dccdcdc43c6e302cfb630d7f8bdc823
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/168777
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Hareen Kancharla <hareen.kancharla@couchbase.com>
    Reviewed-by: Abhijeeth Nuthan <abhijeeth.nuthan@couchbase.com>
    Reviewed-by: Steve Watanabe <steve.watanabe@couchbase.com>

--- src/menelaus_web_rbac.erl
+++ src/menelaus_web_rbac.erl
@@ -1569,13 +1569,10 @@ handle_post_password_policy(Req) ->
                         must_present_value(enforceDigits, digits, Values) ++
                         must_present_value(enforceSpecialChars, special,
                                            Values)}],
-              PrevSettings = ns_config:read_key_fast(password_policy, []),
+              OldSettings = ns_config:read_key_fast(password_policy, []),
               ns_config:set(password_policy, Policy),
-              NewSettings = ns_config:read_key_fast(password_policy, []),
-              event_log:add_log(
-                password_policy_changed,
-                [{old_settings, {PrevSettings}},
-                 {new_settings, {NewSettings}}]),
+              event_log:maybe_add_log_settings_changed(password_policy_changed,
+                                                       OldSettings, Policy, []),
               ns_audit:password_policy(Req, Policy),
               menelaus_util:reply(Req, 200)
       end, Req, form, post_password_policy_validators()).
