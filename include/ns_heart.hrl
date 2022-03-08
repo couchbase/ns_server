91e43c8d6a44c3b4cf0e920b2ceb469117fbb48a
Blair Watt <18262146+udkyo@users.noreply.github.com>
2022-03-08 17:20:29 +0000

    BSL License updates - Couchbase Server Version 7.1
    Change-Id: Iea5123106c0374755b963fca03f2116ff7970857
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/171917
    Tested-by: Blair Watt <blair.watt@couchbase.com>
    Reviewed-by: Steve Watanabe <steve.watanabe@couchbase.com>

--- include/ns_heart.hrl
+++ include/ns_heart.hrl
@@ -1,10 +1,10 @@
 %% @author Couchbase <info@couchbase.com>
 %% @copyright 2011-Present Couchbase, Inc.
 %%
-%% Use of this software is governed by the Business Source License included
-%% in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
-%% in that file, in accordance with the Business Source License, use of this
-%% software will be governed by the Apache License, Version 2.0, included in
-%% the file licenses/APL2.txt.
+%% Use of this software is governed by the Business Source License included in
+%% the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+%% file, in accordance with the Business Source License, use of this software
+%% will be governed by the Apache License, Version 2.0, included in the file
+%% licenses/APL2.txt.
 %%
 -define(HEART_BEAT_PERIOD, 5000).
