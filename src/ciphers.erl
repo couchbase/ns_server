0941ee8fbf82e0dbbf5297f9d9f4e932c5247b46
Murtadha Hubail <murtadha.hubail@couchbase.com>
2022-07-08 16:49:15 +0300

    MB-52872: Add CHACHA20 ciphers to Analytics supported ciphers
    CHACHA20 ciphers are now supported by the OpenJDK version that
    we bundle.
    
    Change-Id: If0bf8d7afd8472cc6165e7f72ab75783f35cd816
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/177285
    Well-Formed: Build Bot <build@couchbase.com>
    Well-Formed: Restriction Checker
    Tested-by: Murtadha Hubail <Murtadha.hubail@couchbase.com>
    Reviewed-by: Dave Finlay <dave.finlay@couchbase.com>

--- src/ciphers.erl
+++ src/ciphers.erl
@@ -277,7 +277,11 @@ supported(cbas) ->
         <<"TLS_ECDH_RSA_WITH_AES_128_CBC_SHA">>,
         <<"TLS_DHE_RSA_WITH_AES_128_CBC_SHA">>,
         <<"TLS_DHE_DSS_WITH_AES_128_CBC_SHA">>,
-        <<"TLS_EMPTY_RENEGOTIATION_INFO_SCSV">>
+        <<"TLS_EMPTY_RENEGOTIATION_INFO_SCSV">>,
+        <<"TLS_CHACHA20_POLY1305_SHA256">>,
+        <<"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256">>,
+        <<"TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256">>,
+        <<"TLS_DHE_RSA_WITH_CHACHA20_POLY1305_SHA256">>
     ].
 
 golang_ciphers({1, 13, 14}) -> golang_ciphers({1, 13, 7});
