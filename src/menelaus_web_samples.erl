couchbase/ns_server

commit 280b29c6037d8688cd989b92ce96d93dab02e9c5

MB-53736: Filter invalid sample buckets serverless
Filter out and actually disable sample buckets that do not work, when
in serverless mode. Everything is intended to still work as normal is
standard "default" profile.

Change-Id: I779b465abf7481d10fa340735dc83448403255ea
Reviewed-on: https://review.couchbase.org/c/ns_server/+/180320
Tested-by: Bryan McCoid <bryan.mccoid@couchbase.com>
Tested-by: Build Bot <build@couchbase.com>
Well-Formed: Build Bot <build@couchbase.com>
Reviewed-by: Steve Watanabe <steve.watanabe@couchbase.com>

src/menelaus_web_samples.erl
@@ -22,6 +22,7 @@
         [reply_json/2,
          reply_json/3]).
 
+-define(COUCHDB_REQUIRED_SAMPLES, ["gamesim-sample", "beer-sample"]).
 -define(SAMPLES_LOADING_TIMEOUT, 120000).
 -define(SAMPLE_BUCKET_QUOTA_MB, 200).
 -define(SAMPLE_BUCKET_QUOTA, 1024 * 1024 * ?SAMPLE_BUCKET_QUOTA_MB).
@@ -39,12 +40,39 @@ handle_get(Req) ->
 
     reply_json(Req, Map).
 
+-spec none_use_couchdb(Samples :: [binary()]) -> boolean().
+none_use_couchdb(Samples) ->
+    lists:all(samples_without_couchdb(_), Samples).
+
+-spec samples_without_couchdb(Name :: binary()) -> Return :: boolean().
+samples_without_couchdb(Name) when is_binary(Name) ->
+    not lists:member(binary_to_list(Name), ?COUCHDB_REQUIRED_SAMPLES).
+
 handle_post(Req) ->
     menelaus_util:assert_is_71(),
     menelaus_web_rbac:assert_no_users_upgrade(),
     case try_decode(mochiweb_request:recv_body(Req)) of
-        {ok, Samples} ->
-            process_post(Req, Samples);
+        {ok, Samples} when is_list(Samples), not is_binary(Samples) ->
+            case config_profile:get_bool({couchdb, disabled})
+                and none_use_couchdb(Samples) of
+                true ->
+                    process_post(Req, Samples);
+                false ->
+                    SampleNames =
+                        lists:map(
+                          fun (FullPath) ->
+                                  list_to_binary(
+                                    filename:basename(FullPath, ".zip"))
+                          end, list_sample_files()),
+                    Err = io_lib:format(
+                            "Attempted to load invalid samples for current configuration profile. "
+                            "Attempted: ~p, Valid: ~p", [Samples, SampleNames]),
+                    reply_json(Req, list_to_binary(Err), 400)
+            end;
+        {ok, _Samples} ->
+            reply_json(
+              Req,
+              list_to_binary("A [list] of names must be specified."), 400);
         {error, Error} ->
             reply_json(Req, list_to_binary(Error), 400)
     end.
@@ -100,8 +128,17 @@ start_loading_sample(Req, Name) ->
 
 list_sample_files() ->
     BinDir = path_config:component_path(bin),
-    filelib:wildcard(filename:join([BinDir, "..", "samples", "*.zip"])).
-
+    AllSamples = filelib:wildcard(
+                   filename:join([BinDir, "..", "samples", "*.zip"])),
+    case config_profile:get_bool({couchdb, disabled}) of
+        true ->
+            lists:filter(
+              ?cut(samples_without_couchdb(
+                     list_to_binary(
+                       filename:basename(_, ".zip")))), AllSamples);
+        false ->
+            AllSamples
+    end.
 
 sample_exists(Name) ->
     BinDir = path_config:component_path(bin),
@@ -140,7 +177,7 @@ check_quota(Samples) ->
     end.
 
 
-check_valid_samples(Samples) when is_list(Samples) ->
+check_valid_samples(Samples) ->
     Errors = [begin
                   case ns_bucket:name_conflict(binary_to_list(Name)) of
                       true ->
@@ -160,9 +197,7 @@ check_valid_samples(Samples) when is_list(Samples) ->
             ok;
         X ->
             X
-    end;
-check_valid_samples(_Samples) ->
-    [{error, list_to_binary("A [list] of names must be specified.")}].
+    end.
 
 format_MB(X) ->
     integer_to_list(misc:ceiling(X / 1024 / 1024)) ++ "MB".

commit 7822cb25341331943f09052a84b26fa6b607d5eb

MB-53736: Filter sample buckets correction
Fixes: 280b29c6037d8688cd989b92ce96d93dab02e9c5 which incorrectly
handles user imput in default profile.

Change-Id: Idf172b03ad8efb783f2a6db2bbceb61dac82b423
Reviewed-on: https://review.couchbase.org/c/ns_server/+/180577
Tested-by: Bryan McCoid <bryan.mccoid@couchbase.com>
Well-Formed: Build Bot <build@couchbase.com>
Reviewed-by: Steve Watanabe <steve.watanabe@couchbase.com>

src/menelaus_web_samples.erl
@@ -42,32 +42,38 @@ handle_get(Req) ->
 
 -spec none_use_couchdb(Samples :: [binary()]) -> boolean().
 none_use_couchdb(Samples) ->
-    lists:all(samples_without_couchdb(_), Samples).
+    lists:all(?cut(samples_without_couchdb(binary_to_list(_))), Samples).
 
--spec samples_without_couchdb(Name :: binary()) -> Return :: boolean().
-samples_without_couchdb(Name) when is_binary(Name) ->
-    not lists:member(binary_to_list(Name), ?COUCHDB_REQUIRED_SAMPLES).
+-spec samples_without_couchdb(Name :: string()) -> Return :: boolean().
+samples_without_couchdb(Name) when is_list(Name) ->
+    not lists:member(string:trim(Name), ?COUCHDB_REQUIRED_SAMPLES).
 
 handle_post(Req) ->
     menelaus_util:assert_is_71(),
     menelaus_web_rbac:assert_no_users_upgrade(),
     case try_decode(mochiweb_request:recv_body(Req)) of
         {ok, Samples} when is_list(Samples), not is_binary(Samples) ->
-            case config_profile:get_bool({couchdb, disabled})
-                and none_use_couchdb(Samples) of
+            case config_profile:get_bool({couchdb, disabled}) of
                 true ->
-                    process_post(Req, Samples);
+                    case none_use_couchdb(Samples) of
+                        true ->
+                            process_post(Req, Samples);
+                        false ->
+                            SampleNames =
+                                lists:map(
+                                  fun (FullPath) ->
+                                          list_to_binary(
+                                            filename:basename(FullPath, ".zip"))
+                                  end, list_sample_files()),
+                            Err =
+                                list_to_binary(
+                                  io_lib:format(
+                                    "Attempted to load invalid samples for current configuration profile. "
+                                    "Attempted: ~p, Valid: ~p", [Samples, SampleNames])),
+                            reply_json(Req, Err, 400)
+                    end;
                 false ->
-                    SampleNames =
-                        lists:map(
-                          fun (FullPath) ->
-                                  list_to_binary(
-                                    filename:basename(FullPath, ".zip"))
-                          end, list_sample_files()),
-                    Err = io_lib:format(
-                            "Attempted to load invalid samples for current configuration profile. "
-                            "Attempted: ~p, Valid: ~p", [Samples, SampleNames]),
-                    reply_json(Req, list_to_binary(Err), 400)
+                    process_post(Req, Samples)
             end;
         {ok, _Samples} ->
             reply_json(
@@ -134,8 +140,7 @@ list_sample_files() ->
         true ->
             lists:filter(
               ?cut(samples_without_couchdb(
-                     list_to_binary(
-                       filename:basename(_, ".zip")))), AllSamples);
+                     filename:basename(_, ".zip"))), AllSamples);
         false ->
             AllSamples
     end.