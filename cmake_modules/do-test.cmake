980bc255551ca527ce57dc07a2f490613c8d1471
Ben Huddleston <ben.huddleston@couchbase.com>
2022-11-10 15:08:00 +0000

    Remove duplicated eunit test command for echo
    We can just store the command in a variable if we also want to print
    it and then we don't have to update it in two different places.
    
    Output:
    
    $ ninja ns_test
    ...
    /Users/benhuddleston/src/ee/install/bin/erl -pa deps/ale/ebin deps/chronicle/ebin deps/enacl/ebin deps/gen_smtp/ebin deps/meck/ebin deps/ns_babysitter/ebin deps/ns_couchdb/ebin deps/triq/ebin ebin .eunit deps/ale/.eunit deps/ns_couchdb/.eunit -pa /Users/benhuddleston/src/ee/build/couchdb/src/couchdb -pa /Users/benhuddleston/src/ee/build/couchdb/src/mochiweb -pa /Users/benhuddleston/src/ee/build/couchdb/src/ejson -pa /Users/benhuddleston/src/ee/build/couchdb/src/couch_index_merger/ebin -noshell -kernel logger [{handler, default, undefined}] -shutdown_time 10000 -sname test-V0UmhxyqrMx8LBck -eval application:start(sasl). -eval
                case t:start() of
                    ok -> init:stop();
                    _ -> init:stop(1)
                end.
    
    Change-Id: I10a963dcbd44ba26bfa55f2a9ae0aacbdfac0d7a
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/182602
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Ben Huddleston <ben.huddleston@couchbase.com>
    Reviewed-by: Abhijeeth Nuthan <abhijeeth.nuthan@couchbase.com>

--- cmake_modules/do-test.cmake
+++ cmake_modules/do-test.cmake
@@ -21,33 +21,28 @@ STRING (REGEX REPLACE "//" "/" eunitdirs "${eunitdirs}")
 STRING (RANDOM LENGTH 16 NODE_NAME_RANDOM)
 SET (NODE_NAME "test-${NODE_NAME_RANDOM}")
 
-# If you update the test command, please also update this echo command
-# (including the silly escaped quotes) so it displays what is
-# invoked. Yes, this is annoying.
-EXECUTE_PROCESS(COMMAND "${CMAKE_COMMAND}" -E echo
-  "${ERL_EXECUTABLE}"
-  -pa ${ebindirs} ${eunitdirs}
-  -pa "${COUCHDB_BIN_DIR}/src/couchdb"
-  -pa "${COUCHDB_BIN_DIR}/src/mochiweb"
-  -pa "${COUCHDB_BIN_DIR}/src/ejson"
-  -pa "${COUCHDB_BIN_DIR}/src/couch_index_merger/ebin"
-  -noshell -kernel logger "[{handler, default, undefined}]" -shutdown_time 10000
-  -sname "${NODE_NAME}"
-  -eval "\"application:start(sasl).\""
-  -eval "\"case t:${TEST_TARGET}() of ok -> init:stop(); _ -> init:stop(1) end.\"")
+SET(TEST_COMMAND "${ERL_EXECUTABLE}"
+        # prefer eunitdirs
+        -pa ${ebindirs} ${eunitdirs}
+        -pa "${COUCHDB_BIN_DIR}/src/couchdb"
+        -pa "${COUCHDB_BIN_DIR}/src/mochiweb"
+        -pa "${COUCHDB_BIN_DIR}/src/ejson"
+        -pa "${COUCHDB_BIN_DIR}/src/couch_index_merger/ebin"
+        -noshell
+        -kernel logger "[{handler, default, undefined}]"
+        -shutdown_time 10000
+        -sname "${NODE_NAME}"
+        -eval "application:start(sasl)."
+        # Need to escape ';' when we pass it to erl or it is treated
+        # as two strings and that doesn't compile.
+        -eval "
+            case t:${TEST_TARGET}() of
+                ok -> init:stop()\;
+                _ -> init:stop(1)
+            end.")
 
-EXECUTE_PROCESS(RESULT_VARIABLE _failure
-  COMMAND "${ERL_EXECUTABLE}"
-  # prefer eunitdirs
-  -pa ${ebindirs} ${eunitdirs}
-  -pa "${COUCHDB_BIN_DIR}/src/couchdb"
-  -pa "${COUCHDB_BIN_DIR}/src/mochiweb"
-  -pa "${COUCHDB_BIN_DIR}/src/ejson"
-  -pa "${COUCHDB_BIN_DIR}/src/couch_index_merger/ebin"
-  -noshell -kernel logger "[{handler, default, undefined}]" -shutdown_time 10000
-  -sname "${NODE_NAME}"
-  -eval "application:start(sasl)."
-  -eval "case t:${TEST_TARGET}() of ok -> init:stop(); _ -> init:stop(1) end.")
+EXECUTE_PROCESS(COMMAND "${CMAKE_COMMAND}" -E echo ${TEST_COMMAND})
+EXECUTE_PROCESS(RESULT_VARIABLE _failure COMMAND ${TEST_COMMAND})
 IF (_failure)
   MESSAGE (FATAL_ERROR "failed running tests")
 ENDIF (_failure)
