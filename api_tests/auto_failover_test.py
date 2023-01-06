8aca98cba138d1bb65cfdb4b3bde480dd22182dc
Neelima Premsankar <neelima.premsankar@couchbase.com>
2023-01-06 20:12:12 +0000

    Revert "MB-54987: Don't set disableMaxCount to true in serverless profile."
    This reverts commit abe2d12845bdcfc25d67784f1f2b1526cb713e0c.
    
    Reason for revert: Once AV-50414 is addressed, maxCount will not be set
    in serverless mode. It is safe to re-enable disableMaxCount to true.
    
    Change-Id: Ic93b4ef250f87b85f4123bf1ad28ac14345a1210
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/183692
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Neelima Premsankar <neelima.premsankar@couchbase.com>
    Reviewed-by: Artem Stemkovski <artem@couchbase.com>

--- api_tests/auto_failover_test.py
+++ api_tests/auto_failover_test.py
@@ -440,3 +440,11 @@ class OnPremAutoFailoverSettingsTest(AutoFailoverSettingsTestBase):
 
     def server_test(self, cluster):
         self.run_combinations()
+
+class ServerlessAutoFailoverSettingsTest(AutoFailoverSettingsTestBase):
+    @staticmethod
+    def requirements():
+        return testlib.ClusterRequirements(serverless=True)
+
+    def server_test(self, cluster):
+        self.run_combinations()
