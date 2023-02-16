4bec355cd8f8cfc975d7c5b61b3ee2327180a1fa
Peter Searby <peter.searby@couchbase.com>
2023-02-16 16:06:30 +0000

    MB-55588: Fix couch_docs_fragmentation for old endpoint
    This commit fixes the calculation for
    /pools/default/buckets/{bucket}/stats/couch_docs_fragmentation
    
    The calculation is now:
    ((couch_docs_file_size - db_history_file_size) - couch_docs_data_size)
     / (couch_docs_file_size - db_history_file_size)
    
    Since this makes the stat dependent on a new stat, which we don't want
    to add to this old endpoint, the calculation has to be moved to
    stat_names_mappings:pre_70_stat_to_prom_query in order to make use of
    kv_ep_db_history_file_size_bytes without exposing it in the endpoint.
    
    Change-Id: I824bb985eadbd96b077fbd67d5b4bb24719602c0
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/186893
    Well-Formed: Restriction Checker
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Peter Searby <peter.searby@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Abhijeeth Nuthan <abhijeeth.nuthan@couchbase.com>
    Reviewed-by: Steve Watanabe <steve.watanabe@couchbase.com>

--- src/menelaus_stats.erl
+++ src/menelaus_stats.erl
@@ -957,8 +957,6 @@ computed_stats_lazy_proplist(_) ->
                             end
                     end,
 
-    DocsFragmentation = Z2(couch_docs_data_size, couch_docs_disk_size,
-                           Fragmentation),
     ViewsFragmentation = Z2(couch_views_data_size, couch_views_disk_size,
                             Fragmentation),
 
@@ -1023,7 +1021,6 @@ computed_stats_lazy_proplist(_) ->
                   ep_dcp_views_backoff, ep_dcp_2i_backoff, ep_dcp_fts_backoff}]],
 
     [{<<"couch_total_disk_size">>, TotalDisk},
-     {<<"couch_docs_fragmentation">>, DocsFragmentation},
      {<<"couch_views_fragmentation">>, ViewsFragmentation},
      {<<"hit_ratio">>, HitRatio},
      {<<"ep_cache_miss_rate">>, EPCacheMissRatio},
