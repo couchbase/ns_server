3dab51a0a10c6f7bc2b3d421d976a0b7d6b0ff3f
Peter Searby <peter.searby@couchbase.com>
2023-05-09 13:49:42 +0100

    Fix cluster tests NumNodes requirement
    When we don't specify a number of connected nodes required for a test
    set, we wish to have all required nodes connected. If there is a
    cluster with an extra node that is not connected, but there are enough
    connected nodes to satisfy the num_nodes requirement, then the cluster
    should meet that requirement.
    
    Allow test set to run on cluster with enough connected nodes and extra
    nodes that aren't connected.
    
    Change-Id: I1815f0285c0af4e438f4223c9ae528008d4e03ea
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/190694
    Tested-by: Peter Searby <peter.searby@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Timofey Barmin <timofey.barmin@couchbase.com>

--- api_tests/testlib/requirements.py
+++ api_tests/testlib/requirements.py
@@ -154,10 +154,10 @@ class NumNodes(Requirement):
 
     def is_met(self, cluster):
         return (len(cluster.nodes) >= self.num_nodes and
-                (self.num_connected is None and
-                 len(cluster.connected_nodes) == len(cluster.nodes)) or
-                (self.num_connected is not None and
-                 len(cluster.connected_nodes) >= self.num_connected))
+                ((self.num_connected is None and
+                  len(cluster.connected_nodes) >= self.num_nodes) or
+                 (self.num_connected is not None and
+                  len(cluster.connected_nodes) >= self.num_connected)))
 
 
 class MemSize(Requirement):
