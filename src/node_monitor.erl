00ed8dd4acc441a2adec55deea2d74f6befcd8af
Ben Huddleston <ben.huddleston@couchbase.com>
2023-05-23 11:11:47 +0100

    MB-55578: Cache local monitors in node_monitor state
    node_monitor refreshes (handle_info(refresh, <>)) currently spend most
    of their time gathering stasues (per fprof):
    
    {[{{health_monitor,handle_message,3},             9,   35.500,    0.111}],
     { {node_monitor,handle_info,2},                  9,   35.500,    0.111},     %
     [{{node_monitor,latest_status,1},                9,   25.353,    0.046},
      {{health_monitor,send_heartbeat,4},             9,    8.915,    0.019},
      {{mb_master,master_node,0},                     9,    0.826,    0.023},
      {{lists,umerge3,3},                             9,    0.161,    0.057},
      {{leader_registry,whereis_name,1},              9,    0.134,    0.048}]}.
    
    {[{{node_monitor,handle_info,2},                  9,   25.353,    0.046}],
     { {node_monitor,latest_status,1},                9,   25.353,    0.046},     %
     [{{health_monitor,local_monitors,0},             9,   19.575,    0.024},
      {{lists,map,2},                                18,    5.732,    0.092}]}.
    
    The majority of this time is spent in the call to
    health_monitor:local_monitors/0 which eventually spends most of its time
    calling ns_cluster_membership:get_snapshot/0 (per fprof, see code for
    full function call stack, omitted for brevity):
    
    {[{{health_monitor,node_monitors,1},              9,   19.509,    0.099}],
     { {health_monitor,supported_services,1},         9,   19.509,    0.099},     %
     [{{ns_cluster_membership,get_snapshot,0},        9,   16.708,    0.019},
      {{cluster_compat_mode,get_compat_version,0},    9,    1.359,    0.048},
      {{health_monitor,'-supported_services/1-lc$^0/1-0-',3},   9,    1.260,    0.056},
      {{health_monitor,supported_services_by_version,1},   9,    0.067,    0.030},
      {{lists,member,2},                              9,    0.016,    0.016}]}.
    
    Services should not change after we initialize a node, and this monitor
    is one which we will want to increase the refresh frequency of to reduce
    failover time, so we should reduce the unnecessary work that it does.
    Cache the local monitors in the node_monitor state such that we no
    longer need to call ns_cluster_membership:get_snapshot/0. This
    information should rarely, if ever, change. Following this change
    node_monitor spends less time in latest_status (per fprof) as it no
    longer calls ns_cluster_membership:get_snapshot/0:
    
    {[{{health_monitor,handle_message,3},            10,   19.538,    0.175}],
     { {node_monitor,handle_info,2},                 10,   19.538,    0.175},     %
     [{{node_monitor,latest_status,2},               10,   10.243,    0.038},
      {{health_monitor,send_heartbeat,4},            10,    7.750,    0.019},
      {{mb_master,master_node,0},                    10,    1.000,    0.023},
      {{lists,umerge3,3},                            10,    0.217,    0.062},
      {{leader_registry,whereis_name,1},             10,    0.153,    0.064}]}.
    
    {[{{node_monitor,handle_info,2},                 10,   10.243,    0.038}],
     { {node_monitor,latest_status,2},               10,   10.243,    0.038},     %
     [{{lists,map,2},                                20,   10.205,    0.083}]}.
    
    Change-Id: Ic1e227f33c05f01deb640655066a28c31389c137
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/192384
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Ben Huddleston <ben.huddleston@couchbase.com>
    Reviewed-by: Artem Stemkovski <artem@couchbase.com>

--- src/node_monitor.erl
+++ src/node_monitor.erl
@@ -18,6 +18,10 @@
 
 -include("ns_common.hrl").
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 -export([start_link/0]).
 -export([get_nodes/0,
          can_refresh/0,
@@ -34,7 +38,18 @@ start_link() ->
     health_monitor:start_link(?MODULE).
 
 init() ->
-    #{}.
+    Monitors = health_monitor:local_monitors(),
+
+    %% We only run non-ns_server monitors for active (non-failed over) nodes,
+    %% subscribe to updates for the local node state. In theory services
+    %% shouldn't change once we setup a node, but subscribe to that too just
+    %% in case.
+    chronicle_compat_events:notify_if_key_changes(
+        [{node, node(), membership},
+         {node, node(), services}], node_changed),
+
+
+    #{local_monitors => Monitors}.
 
 handle_call(get_nodes, _From, MonitorState) ->
     #{nodes := Statuses} = MonitorState,
@@ -58,8 +73,8 @@ handle_cast(Cast, MonitorState) ->
     noreply.
 
 handle_info(refresh, MonitorState) ->
-    #{nodes_wanted := NodesWanted} = MonitorState,
-    Payload = latest_status(NodesWanted),
+    #{nodes_wanted := NodesWanted,
+      local_monitors := LocalMonitors} = MonitorState,
     %% We need to send our status to the node where the auto-failover logic
     %% is running. This is the mb_master node. Normally, this is also
     %% the node where the orchestrator is running.
@@ -93,8 +108,14 @@ handle_info(refresh, MonitorState) ->
                      [M]
              end,
     SendTo = lists:umerge3(Orchestrator, Master, [node()]),
+
+    Payload = latest_status(NodesWanted, LocalMonitors),
     health_monitor:send_heartbeat(?MODULE, SendTo, Payload, MonitorState),
     noreply;
+
+handle_info(node_changed, MonitorState) ->
+    Monitors = health_monitor:local_monitors(),
+    {noreply, MonitorState#{local_monitors => Monitors}};
 
 handle_info(Info, MonitorState) ->
     ?log_warning("Unexpected message ~p when in state:~n~p",
@@ -115,11 +136,11 @@ annotate_status(Status) ->
 %%   [{Node1, [{monitor1, <node1_status>}, {monitor2, <node1_status>}, ...]},
 %%    {Node2, [{monitor1, <node2_status>}, {monitor2, <node2_status>}, ...]},
 %%    ...]
-latest_status(NodesWanted) ->
+latest_status(NodesWanted, LocalMonitors) ->
     AllMonitors = lists:map(fun (Monitor) ->
                                     Module = health_monitor:get_module(Monitor),
                                     {Monitor, Module:get_nodes()}
-                            end, health_monitor:local_monitors()),
+                            end, LocalMonitors),
     lists:map(
       fun (Node) ->
               Status = [{Monitor, node_status(Node, NodesDict)} ||
@@ -152,10 +173,51 @@ health_monitor_test_setup() ->
     meck:new(mb_master),
     meck:expect(mb_master, master_node, fun() -> node() end),
 
+    meck:new(kv_monitor, [passthrough]),
+    meck:expect(kv_monitor,
+        get_nodes,
+        fun() ->
+            dict:append(node(), dict:new(), dict:new())
+        end),
     ok.
 
 health_monitor_t() ->
-    ok.
+    %% Bit of a hack, but this is a test, we can grab the internal state of
+    %% the monitor via sys:get_state to check that we are tracking the
+    %% correct monitors.
+    {state, node_monitor, #{local_monitors := LocalMonitors1}} =
+        sys:get_state(?MODULE),
+
+    ?assertEqual([ns_server], LocalMonitors1),
+
+    %% Should not have called kv_monitor get_nodes yet because we aren't
+    %% tracking a kv_monitor.
+    ?assertNot(meck:called(kv_monitor, get_nodes, [])),
+
+    %% Meck so that when we process the node_changed message we add the
+    %% kv_monitor to the list
+    meck:expect(ns_cluster_membership, should_run_service,
+                fun(_Snapshot, _Service, _Node) ->
+                    true
+                end),
+
+    %% Now lets check that we update the local monitors when we see a
+    %% node_changed message
+    ?MODULE ! node_changed,
+
+    {state, node_monitor, #{local_monitors := LocalMonitors2}} =
+        sys:get_state(?MODULE),
+    ?assertEqual([ns_server, kv], LocalMonitors2),
+
+    %% Send a refresh to check that we now pull the state from the kv_monitor
+    ?MODULE ! refresh,
+
+    %% Do a get_nodes (handle_call) to ensure that we have processed the
+    %% refresh for the next test
+    get_nodes(),
+
+    %% Finally, check that we have now called kv_monitor:get_nodes()
+    ?assert(meck:called(kv_monitor, get_nodes, [])).
 
 health_monitor_test_teardown() ->
     meck:unload(ns_server_monitor),
