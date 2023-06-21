8848c58b818a8cdb140f42fa184aca115e6d7da2
Peter Searby <peter.searby@couchbase.com>
2023-06-21 13:58:29 +0100

    MB-58570: Tidy up NodeAdditionTests
    Makes toggle_n2n handle all nodes in a cluster instead of just one.
    Tidies up logging to be clearer.
    Simplifies n2n_test_base to make it clearer what it does.
    
    Change-Id: I4a428354ed89f7c8457fc7dc70e713809bb5d3de
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/192888
    Reviewed-by: Timofey Barmin <timofey.barmin@couchbase.com>
    Tested-by: Peter Searby <peter.searby@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>

--- cluster_tests/node_addition_tests.py
+++ cluster_tests/node_addition_tests.py
@@ -1,3 +1,17 @@
+# @author Couchbase <info@couchbase.com>
+# @copyright 2023 Couchbase, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import testlib
 from testlib import ClusterRequirements
 
@@ -6,26 +20,26 @@ class NodeAdditionTests(testlib.BaseTestSet):
 
     def __init__(self, cluster):
         super().__init__(cluster)
+        self.disconnected_nodes = None
         self.req_num_nodes = None
 
     def setup(self, cluster):
         self.req_num_nodes = len(cluster.nodes)
+        self.disconnected_nodes = [node for node in cluster.nodes
+                                   if node not in cluster.connected_nodes]
         testlib.delete_all_buckets(cluster)
 
     def teardown(self, cluster):
         pass
 
     def test_teardown(self, cluster):
-        print("Tearing down cluster")
+        print("Removing all but the first node")
         # Rebalance the cluster and remove all but one node
-        cluster.rebalance(cluster.nodes[1:], wait=True, verbose=True)
-        resp = testlib.get_succ(cluster, "/pools/default")
-        print([node["hostname"] for node in resp.json()["nodes"]])
-        if len(resp.json()["nodes"]) != 1:
-            print(f'Length of nodes: {len(resp.json()["nodes"])}')
-            print([node["hostname"] for node in resp.json()["nodes"]])
-            raise ValueError("More than one node in cluster after test "
-                             "teardown")
+        cluster.rebalance(cluster.connected_nodes[1:], wait=True, verbose=True)
+
+        # Verify that the cluster has the correct number of nodes.
+        # Note that if this fails then we will cause future testsets to fail.
+        assert_cluster_size(cluster, 1)
         cluster.wait_nodes_up()
 
     @staticmethod
@@ -35,94 +49,87 @@ class NodeAdditionTests(testlib.BaseTestSet):
                 ClusterRequirements(num_nodes=3, num_connected=1,
                                     afamily="ipv6")]
 
-    def joinCluster(self, cluster):
-        for node in cluster.nodes[1:]:
-            cluster.do_join_cluster(node)
-        cluster.rebalance(wait=True)
-        resp = testlib.get_succ(cluster, "/pools/default")
-
-        return len(resp.json()["nodes"]) == self.req_num_nodes, \
-            f'Length of nodes: {len(resp.json()["nodes"])}'
-
-    def addNode(self, cluster):
-        for node in cluster.nodes[1:]:
-            cluster.add_node(node)
-        cluster.rebalance(wait=True)
-        resp = testlib.get_succ(cluster, "/pools/default")
-
-        return len(resp.json()["nodes"]) == self.req_num_nodes, \
-            f'Length of nodes: {len(resp.json()["nodes"])}'
-
-    def check_all_n2n(self, cluster, enable):
-        # Check that all the nodes have node-to-node encryption.
-        r = testlib.get_succ(cluster, "/pools/nodes")
-        return all([node["nodeEncryption"] == enable
-                    for node in r.json()["nodes"]])
-
-    def toggle_n2n(self, cluster, enable=True):
-        """
-        Helper function to enable/disable node to node encryption
-        :param cluster: From the cluster tuple in testlib.py
-        :param enable: Whether node to node encryption should be enabled.
-        """
-
-        # Need to disable autoFailover before other settings can change
-        testlib.post_succ(cluster, "/settings/autoFailover",
-                          data={"enabled": "false"})
-
-        # Create an external listener
-        testlib.post_succ(cluster,
-                          "/node/controller/enableExternalListener",
-                          data={"nodeEncryption": "on" if enable else "off"})
-
-        # Change the node-to-node encryption settings
-        testlib.post_succ(cluster, "/node/controller/setupNetConfig",
-                          data={"nodeEncryption": "on" if enable else "off"})
-        # Disable any unused listeners
-        testlib.post_succ(cluster,
-                          "/node/controller/disableUnusedExternalListeners")
-
-        # Re-enable autoFailover.
-        testlib.post_succ(cluster, "/settings/autoFailover",
-                          data={"enabled": "true",
-                                "timeout": 120})
-
-        return self.check_all_n2n(cluster, enable)
-
     def n2n_test_base(self, cluster, method, enable: bool):
         """
         A base structure of testing node to node encryption with adding nodes.
-        :param cluster: Cluster class.
+        :param cluster: Cluster object to send requests to
         :param method: which REST request to add nodes:
-                       self.addNode/self.joinCluster
+                       cluster.add_node/cluster.do_join_cluster
         :param enable: whether node-to-node encryption is enabled or disabled
         """
         # Change the node-to-node settings.
-        change_n2n = self.toggle_n2n(cluster, enable=enable)
-        if not change_n2n:
-            assert False, f"Changing node-to-node encryption failed"
-        test_result, test_message = method(cluster)
+        toggle_n2n(cluster, enable=enable)
 
-        check_n2n = self.check_all_n2n(cluster, enable)
+        # Test a node addition method
+        for node in self.disconnected_nodes:
+            method(node)
+        cluster.rebalance(wait=True)
 
-        if not check_n2n:
-            assert False, \
-                f"Expected node-to-node encryption to be {enable}, " \
-                f"but received " \
-                f"{[node['nodeEncryption'] for node in r.json()['nodes']]}"
-        assert test_result, test_message
+        # Verify that the cluster has the correct number of nodes
+        assert_cluster_size(cluster, self.req_num_nodes)
+
+        # Verify all nodes, including the new node, have the correct n2n setting
+        assert_n2n(cluster, enable)
 
     def n2n_off_addnode_test(self, cluster):
-        self.n2n_test_base(cluster, self.addNode, False)
+        self.n2n_test_base(cluster, cluster.add_node, False)
 
     def n2n_on_addnode_test(self, cluster):
-        self.n2n_test_base(cluster, self.addNode, True)
+        self.n2n_test_base(cluster, cluster.add_node, True)
 
     def n2n_off_joincluster_test(self, cluster):
-        self.n2n_test_base(cluster, self.joinCluster, False)
+        self.n2n_test_base(cluster, cluster.do_join_cluster, False)
 
     def n2n_on_joincluster_test(self, cluster):
-        self.n2n_test_base(cluster, self.joinCluster, True)
+        self.n2n_test_base(cluster, cluster.do_join_cluster, True)
 
 
+# Assert that all nodes have node-to-node encryption enabled/disabled
+def assert_n2n(cluster, expected_value):
+    r = testlib.get_succ(cluster, "/pools/default")
+    bad_nodes = [node['hostname'] for node in r.json()['nodes']
+                 if node['nodeEncryption'] != expected_value]
+    assert len(bad_nodes) == 0, \
+        f"Expected nodeEncryption to be {expected_value} for all nodes, " \
+        f"but got {not expected_value} for the following nodes: {bad_nodes}"
 
+
+# Assert that the cluster has the expected size
+def assert_cluster_size(cluster, expected_size):
+    resp = testlib.get_succ(cluster, "/pools/default")
+    nodes = [node["hostname"] for node in resp.json()["nodes"]]
+    assert len(nodes) == expected_size, \
+        f"Wrong number of nodes in cluster. Expected {expected_size} " \
+        f"nodes, found the following set of nodes: {nodes}"
+
+
+def toggle_n2n(cluster, enable=True):
+    """
+    Helper function to enable/disable node to node encryption for all nodes
+    :param cluster: Cluster object to send requests to
+    :param enable: Whether node to node encryption should be enabled.
+    """
+    # Need to disable autoFailover before other settings can change
+    testlib.post_succ(cluster, "/settings/autoFailover",
+                      data={"enabled": "false"})
+
+    for node in cluster.connected_nodes:
+        # Create an external listener
+        testlib.post_succ(node, "/node/controller/enableExternalListener",
+                          data={"nodeEncryption": "on"
+                                if enable else "off"})
+
+        # Change the node-to-node encryption settings
+        testlib.post_succ(node, "/node/controller/setupNetConfig",
+                          data={"nodeEncryption": "on"
+                                if enable else "off"})
+        # Disable any unused listeners
+        testlib.post_succ(node,
+                          "/node/controller/disableUnusedExternalListeners")
+
+    # Re-enable autoFailover.
+    testlib.post_succ(cluster, "/settings/autoFailover",
+                      data={"enabled": "true",
+                            "timeout": 120})
+
+    assert_n2n(cluster, enable)
