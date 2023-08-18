0edf2d00a2513db0d2b95d836347a4f789c40f2f
Artem Stemkovski <astemkov@gmail.com>
2023-08-18 16:50:59 -0700

    MB-58303 get rid of unused stuff in chronicle_compat_events
    the final goal is to get rid of this module altogether, but
    for now just pick the low hanging fruit
    
    Change-Id: I832557f9c1e73c07bc31414bf49a0e891d69204c
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/195934
    Tested-by: Artem Stemkovski <artem@couchbase.com>
    Reviewed-by: Neelima Premsankar <neelima.premsankar@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>

--- src/chronicle_compat_events.erl
+++ src/chronicle_compat_events.erl
@@ -12,8 +12,6 @@
 -behaviour(gen_server2).
 
 -export([start_link/0,
-         hush_chronicle/0,
-         resume_chronicle/0,
          event_manager/0,
          kv_event_manager/0,
          subscribe/1,
@@ -27,16 +25,8 @@
 -include("ns_common.hrl").
 -include("cut.hrl").
 
--record(state, {chronicle_events_pid, event_manager, kv_event_manager}).
-
 start_link() ->
     gen_server2:start_link({local, ?MODULE}, ?MODULE, [], []).
-
-resume_chronicle() ->
-    gen_server2:call(?MODULE, resume_chronicle).
-
-hush_chronicle() ->
-    gen_server2:call(?MODULE, hush_chronicle).
 
 sync() ->
     gen_server2:call(?MODULE, sync,
@@ -48,10 +38,8 @@ init([]) ->
     {ok, EventManager} = gen_event:start_link({local, event_manager()}),
     {ok, KVEventManager} = gen_event:start_link({local, kv_event_manager()}),
     subscribe_to_ns_config_events(EventManager),
-    Pid = subscribe_to_chronicle_events(EventManager, KVEventManager),
-    {ok, #state{chronicle_events_pid = Pid,
-                event_manager = EventManager,
-                kv_event_manager = KVEventManager}}.
+    subscribe_to_chronicle_events(EventManager, KVEventManager),
+    {ok, []}.
 
 event_manager() ->
     chronicle_compat_event_manager.
@@ -78,23 +66,9 @@ subscribe_to_ns_config_events(EventManager) ->
               ok
       end).
 
-handle_call(hush_chronicle, _From,
-            #state{chronicle_events_pid = undefined} = State) ->
-    {reply, ok, State};
-handle_call(hush_chronicle, _From,
-            #state{chronicle_events_pid = Pid} = State) ->
-    ns_pubsub:unsubscribe(Pid),
-    {reply, ok, State#state{chronicle_events_pid = undefined}};
-handle_call(resume_chronicle, _From,
-            #state{chronicle_events_pid = undefined,
-                   event_manager = EventManager,
-                   kv_event_manager = KVEventManager} = State) ->
-    Pid = subscribe_to_chronicle_events(EventManager, KVEventManager),
-    {reply, ok, State#state{chronicle_events_pid = Pid}};
-handle_call(sync, _From, #state{chronicle_events_pid = Pid} = State) ->
+handle_call(sync, _From, State) ->
     ns_config:sync_announcements(),
-    Pid == undefined orelse
-        gen_event:which_handlers(chronicle_kv:event_manager(kv)),
+    gen_event:which_handlers(chronicle_kv:event_manager(kv)),
     gen_event:which_handlers(event_manager()),
     {reply, ok, State}.
 
