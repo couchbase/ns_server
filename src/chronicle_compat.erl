e56fd5b65a03d03afeb4abec7232e0c717aefd01
Artem Stemkovski <astemkov@gmail.com>
2023-08-22 16:01:16 -0700

    MB-58303 remove some dead code from chronicle_compat
    so far we cannot remove the whole module, because
    - we need to move some helpers to chronicle proper
    - we still need a compat layer between chronicle and chronicle_dup
    - one fun in this module is called by rpc from other nodes
    
    Change-Id: Ie91b3bf89ae12ff775ffc5ce69c2b403db3b6732
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/195943
    Reviewed-by: Hareen Kancharla <hareen.kancharla@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Artem Stemkovski <artem@couchbase.com>

--- src/chronicle_compat.erl
+++ src/chronicle_compat.erl
@@ -24,7 +24,6 @@
          transaction/2,
          txn/1,
          txn/2,
-         txn/3,
          ro_txn/2,
          txn_get/2,
          txn_get_many/2,
@@ -47,15 +46,13 @@ backend() ->
     ns_config:read_key_fast(chronicle_backend, chronicle).
 
 get(Key, Opts) ->
-    get(ns_config:latest(), Key, Opts).
+    get(direct, Key, Opts).
 
-get(direct, Key, Opts) ->
-    get(ns_config:latest(), Key, Opts);
-get(Config, Key, #{required := true}) ->
-    {ok, Value} = get(Config, Key, #{}),
+get(Source, Key, #{required := true}) ->
+    {ok, Value} = get(Source, Key, #{}),
     Value;
-get(Config, Key, #{default := Default}) ->
-    case get(Config, Key, #{}) of
+get(Source, Key, #{default := Default}) ->
+    case get(Source, Key, #{}) of
         {error, not_found} ->
             Default;
         {ok, Value} ->
@@ -68,140 +65,75 @@ get(Snapshot, Key, #{}) when is_map(Snapshot) ->
         error ->
             {error, not_found}
     end;
-get(Config, Key, #{}) ->
-    case backend() of
-        chronicle ->
-            case ns_node_disco:couchdb_node() =:= node() of
-                true ->
-                    case ns_couchdb_chronicle_dup:lookup(Key) of
-                        [{Key, {Value, _Rev}}] ->
-                            {ok, Value};
-                        [] ->
-                            {error, not_found}
-                    end;
-                false ->
-                    case chronicle_kv:get(kv, Key, #{}) of
-                        {ok, {V, _R}} ->
-                            {ok, V};
-                        Error ->
-                            Error
-                    end
-            end;
-        ns_config ->
-            case ns_config:search(Config, Key) of
-                {value, Value} ->
+get(_Config, Key, #{}) ->
+    case ns_node_disco:couchdb_node() =:= node() of
+        true ->
+            case ns_couchdb_chronicle_dup:lookup(Key) of
+                [{Key, {Value, _Rev}}] ->
                     {ok, Value};
-                false ->
+                [] ->
                     {error, not_found}
+            end;
+        false ->
+            case chronicle_kv:get(kv, Key, #{}) of
+                {ok, {V, _R}} ->
+                    {ok, V};
+                Error ->
+                    Error
             end
     end.
 
 set(Key, Value) ->
-    case backend() of
-        chronicle ->
-            case chronicle_kv:set(kv, Key, Value) of
-                {ok, _} ->
-                    ok;
-                Error ->
-                    Error
-            end;
-        ns_config ->
-            ns_config:set(Key, Value),
-            ok
+    case chronicle_kv:set(kv, Key, Value) of
+        {ok, _} ->
+            ok;
+        Error ->
+            Error
     end.
 
 set_multiple([]) ->
     ok;
 set_multiple(List) ->
-    case backend() of
-        chronicle ->
-            Fun = fun (_) -> {commit, [{set, K, V} || {K, V} <- List]} end,
-            case chronicle_kv:transaction(kv, [], Fun, #{}) of
-                {ok, _} ->
-                    ok;
-                Error ->
-                    Error
-            end;
-        ns_config ->
-            ns_config:set(List)
+    Fun = fun (_) -> {commit, [{set, K, V} || {K, V} <- List]} end,
+    case chronicle_kv:transaction(kv, [], Fun, #{}) of
+        {ok, _} ->
+            ok;
+        Error ->
+            Error
     end.
 
 transaction(Keys, Fun) ->
-    transaction(backend(), Keys, Fun).
-
-transaction(Type, Keys, Fun) ->
-    txn(Type, ?cut(Fun(txn_get_many(Keys, _))), #{}).
+    txn(?cut(Fun(txn_get_many(Keys, _)))).
 
 txn(Fun) ->
     txn(Fun, #{}).
 
 txn(Fun, Opts) ->
-    txn(backend(), Fun, Opts).
-
-txn(chronicle, Fun, Opts) ->
-    chronicle_kv:txn(kv, ?cut(Fun({chronicle, _})), Opts);
-txn(ns_config, Fun, _Opts) ->
-    TXNRV =
-        ns_config:run_txn(
-          fun (Cfg, SetFn) ->
-                  BuildCommit =
-                      lists:foldl(
-                        fun ({set, K, V}, Acc) -> SetFn(K, V, Acc) end,
-                        Cfg, _),
-                  case Fun({ns_config, Cfg}) of
-                      {abort, _} = Abort ->
-                          Abort;
-                      {commit, Sets, Extra} ->
-                          {commit, BuildCommit(Sets), Extra};
-                      {commit, Sets} ->
-                          {commit, BuildCommit(Sets)}
-                  end
-          end),
-    case TXNRV of
-        {commit, _} ->
-            {ok, no_rev};
-        {commit, _, Extra} ->
-            {ok, no_rev, Extra};
-        {abort, Error} ->
-            Error;
-        retry_needed ->
-            erlang:error(exceeded_retries)
-    end.
+    chronicle_kv:txn(kv, ?cut(Fun({chronicle, _})), Opts).
 
 ro_txn(Body, Opts) ->
     Type =
-        case {backend(), ns_node_disco:couchdb_node() == node()} of
-            {chronicle, true} ->
+        case ns_node_disco:couchdb_node() == node() of
+            true ->
                 couchdb;
-            {Backend, _} ->
-                Backend
+            false ->
+                chronicle
         end,
     ro_txn(Type, Body, Opts).
 
 ro_txn(chronicle, Body, Opts) ->
     chronicle_kv:ro_txn(kv, ?cut(Body({chronicle, _})), Opts);
-ro_txn(ns_config, Body, #{ns_config := Config}) ->
-    {ok, {Body({ns_config, Config}), no_rev}};
-ro_txn(ns_config, Body, _Opts) ->
-    {ok, {Body({ns_config, ns_config:get()}), no_rev}};
 ro_txn(couchdb, Body, #{}) ->
     {ok, {ns_couchdb_chronicle_dup:ro_txn(?cut(Body({couchdb, _}))), no_rev}}.
 
 txn_get(K, {chronicle, Txn}) ->
     chronicle_kv:txn_get(K, Txn);
-txn_get(K, {ns_config, Config}) ->
-    case ns_config:search(Config, K) of
-        {value, V} ->
-            {ok, {V, no_rev}};
-        false ->
-            {error, not_found}
-    end;
 txn_get(K, {couchdb, TxnGet}) ->
     TxnGet(K).
 
 txn_get_many(Keys, {chronicle, Txn}) ->
     chronicle_kv:txn_get_many(Keys, Txn);
-txn_get_many(Keys, Txn) ->
+txn_get_many(Keys, {couchdb, _} = Txn) ->
     lists:foldl(
       fun (K, Acc) ->
               case txn_get(K, Txn) of
@@ -234,12 +166,7 @@ pull() ->
     pull(ns_config_rep:get_timeout(pull)).
 
 pull(Timeout) ->
-    case backend() of
-        ns_config ->
-            ok;
-        chronicle ->
-            do_pull(Timeout)
-    end.
+    do_pull(Timeout).
 
 do_pull(Timeout) ->
     ok = chronicle_kv:sync(kv, Timeout).
@@ -261,24 +188,14 @@ remote_pull(Nodes, Timeout) ->
 config_sync(Type, Nodes) ->
     config_sync(Type, Nodes, ns_config_rep:get_timeout(Type)).
 
-config_sync(pull, Nodes, Timeout) ->
-    case backend() of
-        ns_config ->
-            ns_config_rep:pull_remotes(Nodes, Timeout);
-        chronicle ->
-            do_pull(Timeout)
-    end;
+config_sync(pull, _Nodes, Timeout) ->
+    do_pull(Timeout);
 config_sync(push, Nodes, Timeout) ->
-    case backend() of
-        ns_config ->
-            ns_config_rep:ensure_config_seen_by_nodes(Nodes, Timeout);
-        chronicle ->
-            case remote_pull(Nodes, Timeout) of
-                ok ->
-                    ok;
-                {error, {remote_pull_failed, BadResults}} ->
-                    {error, BadResults}
-            end
+    case remote_pull(Nodes, Timeout) of
+        ok ->
+            ok;
+        {error, {remote_pull_failed, BadResults}} ->
+            {error, BadResults}
     end.
 
 node_keys(Node, Buckets) ->
