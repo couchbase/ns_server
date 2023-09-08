17c4fbd1f1ed01ee010ff5de4b4df1984587654b
Steve Watanabe <steve.watanabe@couchbase.com>
2023-09-08 11:52:36 -0700

    MB-58580 Update queryNodeQuota only when trinity
    Currently we try to update queryNodeQuota in the query setting manager
    on a /pools/default POST. This cannot be done if the cluster compat mode
    is not trinity. This case occurs when --pretend-version is specified for
    cluster_run. With this change the update won't occur if the cluster
    compat mode isn't trinity.
    
    Change-Id: Ib1ceabfdad5bdac092425fae4f179018d4fc7831
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/196876
    Reviewed-by: Bryan McCoid <bryan.mccoid@couchbase.com>
    Tested-by: Steve Watanabe <steve.watanabe@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>

--- src/menelaus_web_pools.erl
+++ src/menelaus_web_pools.erl
@@ -530,16 +530,23 @@ handle_pool_settings_post_body(Req, Config, Values) ->
             false ->
                 false
         end,
-    QueryQuotaSupplied =
-        case lists:keyfind(queryNodeQuota, 1, Values) of
-            false -> 0;
-            {_, QueryQuota} -> QueryQuota
-        end,
-    NewNodeQuotaList =
-        [{queryNodeQuota, QueryQuotaSupplied} |
-         proplists:delete(queryNodeQuota,
-                          query_settings_manager:get(generalSettings))],
-    query_settings_manager:update(generalSettings, NewNodeQuotaList),
+
+    case cluster_compat_mode:is_cluster_trinity() of
+        true ->
+            QueryQuotaSupplied =
+                case lists:keyfind(queryNodeQuota, 1, Values) of
+                    false -> 0;
+                    {_, QueryQuota} -> QueryQuota
+                end,
+            NewNodeQuotaList =
+                [{queryNodeQuota, QueryQuotaSupplied} |
+                 proplists:delete(queryNodeQuota,
+                                  query_settings_manager:get(generalSettings))],
+                query_settings_manager:update(generalSettings,
+                                              NewNodeQuotaList);
+        false ->
+            ok
+    end,
 
     NameSupplied =
         case lists:keyfind(clusterName, 1, Values) of
