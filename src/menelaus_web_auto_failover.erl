9cd259da00b7b38e18a1f9b6102a1a181029d45c
Bryan McCoid <bryan.mccoid@couchbase.com>
2024-01-08 15:39:45 -0800

    MB-60175: Don't reset canAbortRebalance when ...
    ... disabling auto-failover. For some reason, the code was written to
    disable canAbortRebalance whenever we disable auto-failover and that
    value will stick even if it's later enabled again. This would cause the
    value to flip-flop without the user ever doing anything to cause it.
    This is very confusing so this change should make the behavior more
    consistent.
    
    Change-Id: If11430db36bd4bb77621a3ceff2d9d8e0e1a4511
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/203560
    Reviewed-by: Neelima Premsankar <neelima.premsankar@couchbase.com>
    Tested-by: Bryan McCoid <bryan.mccoid@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>

--- src/menelaus_web_auto_failover.erl
+++ src/menelaus_web_auto_failover.erl
@@ -12,6 +12,10 @@
 -include("cut.hrl").
 -include("ns_common.hrl").
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 -export([handle_settings_get/1,
          handle_settings_post/1,
          handle_settings_reset_count/1,
@@ -110,7 +114,7 @@ handle_settings_post_validated(Req, Props) ->
             auto_failover:enable(Timeout, MaxCount, Extras),
             ns_audit:enable_auto_failover(Req, Timeout, MaxCount, Extras);
         false ->
-            auto_failover:disable(disable_extras(Config)),
+            auto_failover:disable(disable_disk_failover(Config)),
             ns_audit:disable_auto_failover(Req)
     end,
     reply(Req, 200).
@@ -311,13 +315,11 @@ get_extra_settings(Config) ->
             []
     end.
 
-disable_extras(Config) ->
+disable_disk_failover(Config) ->
     case cluster_compat_mode:is_enterprise() of
         true ->
             {_, CurrTP} = get_failover_on_disk_issues(Config),
-            lists:flatten(
-              [disable_failover_on_disk_issues(CurrTP),
-               [{?CAN_ABORT_REBALANCE_CONFIG_KEY, false}]]);
+            disable_failover_on_disk_issues(CurrTP);
         false ->
             []
     end.
@@ -325,5 +327,20 @@ disable_extras(Config) ->
 config_upgrade_to_72(Config) ->
     [{set, ?ROOT_CONFIG_KEY,
       auto_failover:get_cfg(Config) ++
-            [{?FAILOVER_PRESERVE_DURABILITY_MAJORITY_CONFIG_KEY,
-              ?FAILOVER_PRESERVE_DURABILITY_MAJORITY_DEFAULT}]}].
+          [{?FAILOVER_PRESERVE_DURABILITY_MAJORITY_CONFIG_KEY,
+            ?FAILOVER_PRESERVE_DURABILITY_MAJORITY_DEFAULT}]}].
+
+-ifdef(TEST).
+dont_flipflop_can_abort_rebalance_test() ->
+    meck:new(cluster_compat_mode),
+    meck:expect(cluster_compat_mode, is_enterprise, fun () -> true end),
+    BaseConfig = [{?DATA_DISK_ISSUES_CONFIG_KEY, [{enabled, true},
+                                                  {timePeriod, 666}]}],
+    Out = disable_disk_failover(BaseConfig),
+    Result = [{?DATA_DISK_ISSUES_CONFIG_KEY,
+               [{enabled, false}, {timePeriod, 666}]}],
+    ?assertEqual(Out, Result),
+    Config1 = [{?CAN_ABORT_REBALANCE_CONFIG_KEY, true} | BaseConfig],
+    Out2 = disable_disk_failover(Config1),
+    ?assertEqual(Out2, Result).
+-endif.
