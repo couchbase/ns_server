17bbe6f4eb352b84ec5bdd97dff07fd5121a4595
Steve Watanabe <steve.watanabe@couchbase.com>
2024-03-07 11:40:53 -0800

    MB-68545[BP]: Fix yaml:encode test
    Note: this BP depends on Erlang/OTP 26, but since it fixes a commented
    out test, it is fine to upgrade to OTP 26 before merging this.
    
    Original commit message below:
    -----------------------------
    
    The maps:to_list is non-deterministic wrt the order in the list of the
    items. In order to get deterministic ordering we use an ordered maps
    iterator.
    
    Change-Id: Ia79245e5eddcedf158977885fabfb3b771602401
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/233914
    Tested-by: Peter Searby <peter.searby@couchbase.com>
    Well-Formed: Restriction Checker
    Reviewed-by: Steve Watanabe <steve.watanabe@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- src/yaml.erl
+++ src/yaml.erl
@@ -65,7 +65,7 @@ format(#{} = Map) ->
                   [[KBin, ":"]] ++
                   ["  " ++ L || L <- MultiLine]
           end
-      end, maps:to_list(Map));
+      end, maps:to_list(maps:iterator(Map, ordered)));
 format([]) ->
     [];
 format([El | Tail]) ->
@@ -86,9 +86,7 @@ format(A) when is_atom(A) ->
 format(B) when is_binary(B) ->
     [["'", B, "'"]].
 
-%% XXX: disable as the last test is non-determistic due to maps:to_list being
-%% arbitrary in its results.
--ifdef(TESTDisabled).
+-ifdef(TEST).
 encode_test() ->
     ?assertEqual(encode([]), <<>>),
     ?assertEqual(encode(#{}), <<>>),
@@ -151,16 +149,16 @@ encode_test() ->
                    "        target_label: 'name'\n"
                    "    metrics_path: '/_prometheusMetrics'\n"
                    "    relabel_configs:\n"
-                   "      - source_labels:\n"
-                   "          - '__address__'\n"
-                   "        target_label: 'instance'\n"
-                   "        regex: '127\\.0\\.0\\.1:9000'\n"
+                   "      - regex: '127\\.0\\.0\\.1:9000'\n"
                    "        replacement: 'ns_server'\n"
-                   "      - source_labels:\n"
+                   "        source_labels:\n"
                    "          - '__address__'\n"
                    "        target_label: 'instance'\n"
-                   "        regex: '127\\.0\\.0\\.1:9200'\n"
+                   "      - regex: '127\\.0\\.0\\.1:9200'\n"
                    "        replacement: 'fts'\n"
+                   "        source_labels:\n"
+                   "          - '__address__'\n"
+                   "        target_label: 'instance'\n"
                    "    static_configs:\n"
                    "      - targets:\n"
                    "          - '127.0.0.1:9000'\n"
