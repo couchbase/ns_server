54e3079f6dc601e7515521820b9b5c0468366b6b
Timofey Barmin <timofey.barmin@couchbase.com>
2024-03-28 16:33:54 -0700

    MB-60555: Use enacl from couchbasedeps/enacl
    Change-Id: I6d033c7a9d3957a5676a8b6c1fbe4af5d1c297a9
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/207849
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Ben Huddleston <ben.huddleston@couchbase.com>
    Tested-by: Timofey Barmin <timofey.barmin@couchbase.com>

--- deps/enacl/bench/funcall_enacl.d
+++
@@ -1,9 +0,0 @@
-erlang*:::nif-entry
-{
-	funcall_entry_ts[cpu, copyinstr(arg1)] = timestamp;
-}
-
-erlang*:::nif-return
-{
-	@time[cpu, copyinstr(arg1)] = lquantize((timestamp - funcall_entry_ts[cpu, copyinstr(arg1)] ), 0, 60000, 1000);
-}
