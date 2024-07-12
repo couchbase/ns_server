05ed2d8baf200314bcb1a97fbb11bdf31c0d1f7b
Bryan McCoid <bryan.mccoid@couchbase.com>
2024-07-12 15:36:22 -0700

    MB-62576: [cluster-test] create cbcollect_info tests
    Create a basic test that runs cbcollect_info on a 3 node cluster, and
    determines that it completed successfully.
    
    Another test that actually runs an analytics_debug.log through the
    cbcollect redaction.
    
    Change-Id: I52043e0054f869801ca7dbcf81f0a77962a50514
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/212690
    Reviewed-by: Steve Watanabe <steve.watanabe@couchbase.com>
    Tested-by: Bryan McCoid <bryan.mccoid@couchbase.com>

--- cluster_tests/run.py
+++ cluster_tests/run.py
@@ -69,7 +69,8 @@ from testsets import \
     web_settings_tests, \
     rest_eject_test, \
     node_remap_tests, \
-    services_topology_tests
+    services_topology_tests, \
+    cbcollect_tests
 
 tmp_cluster_dir = os.path.join(scriptdir, "test_cluster_data")
 
