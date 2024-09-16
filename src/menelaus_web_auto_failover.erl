ad96d5099de76a32253cabc7c8318da37bd224fe
Ben Huddleston <ben.huddleston@couchbase.com>
2024-09-16 10:39:51 +0100

    MB-60175/MB-50476: Remove redundant can_abort_rebalance test
    The test is trying to verify that the setting is retained when disabling
    auto-failover, at least, that's what the commit message implies. The
    test doesn't actually appear to do this, and this behaviour should be
    covered by the auto_failover_test cluster_test suite.
    
    Change-Id: Id6420e4e8ccf058111705881c9330bd08fa554f6
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/216243
    Reviewed-by: Neelima Premsankar <neelima.premsankar@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Ben Huddleston <ben.huddleston@couchbase.com>

--- src/menelaus_web_auto_failover.erl
+++ src/menelaus_web_auto_failover.erl
@@ -366,18 +366,3 @@ config_upgrade_to_72(Config) ->
       auto_failover:get_cfg(Config) ++
           [{?FAILOVER_PRESERVE_DURABILITY_MAJORITY_CONFIG_KEY,
             ?FAILOVER_PRESERVE_DURABILITY_MAJORITY_DEFAULT}]}].
-
--ifdef(TEST).
-dont_flipflop_can_abort_rebalance_test() ->
-    meck:new(cluster_compat_mode),
-    meck:expect(cluster_compat_mode, is_enterprise, fun () -> true end),
-    BaseConfig = [{?DATA_DISK_ISSUES_CONFIG_KEY, [{enabled, true},
-                                                  {timePeriod, 666}]}],
-    Out = disable_disk_failover(BaseConfig),
-    Result = [{?DATA_DISK_ISSUES_CONFIG_KEY,
-               [{enabled, false}, {timePeriod, 666}]}],
-    ?assertEqual(Out, Result),
-    Config1 = [{?CAN_ABORT_REBALANCE_CONFIG_KEY, true} | BaseConfig],
-    Out2 = disable_disk_failover(Config1),
-    ?assertEqual(Out2, Result).
--endif.
