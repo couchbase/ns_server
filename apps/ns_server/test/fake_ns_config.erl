1b2902a39308f71ac65e7eee95db0623ed66c87c
Ben Huddleston <ben.huddleston@couchbase.com>
2024-10-18 15:43:27 +0100

    Expose fake_ns_config:get_ets_snapshot/0
    It's useful sometimes to dump the entire config whilst writing tests.
    
    Change-Id: I87203464effeac9bb9a5c98fa01a70e542273f14
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/218203
    Tested-by: Ben Huddleston <ben.huddleston@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Peter Searby <peter.searby@couchbase.com>

--- apps/ns_server/test/fake_ns_config.erl
+++ apps/ns_server/test/fake_ns_config.erl
@@ -50,7 +50,8 @@
          teardown/0,
          update_snapshot/1,
          update_snapshot/2,
-         delete_key/1]).
+         delete_key/1,
+         get_ets_snapshot/0]).
 
 %% Helper function API
 -export([setup_cluster_compat_version/1]).
