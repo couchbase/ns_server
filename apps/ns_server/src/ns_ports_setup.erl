7d538626573ca4bc1a5f44bc6dcd286e4511795a
Matt Hall <matt.hall@couchbase.com>
2024-11-11 09:53:10 +0000

    MB-64170 Only run `cbcontbk` on EE
    It is an enterprise-only feature and the binary will not be built for
    CE.
    
    Change-Id: I7fd74dd4a77d691a601624c8a2016f7f5f1ebca0
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/219359
    Tested-by: Matt Hall <matt.hall@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Peter Searby <peter.searby@couchbase.com>

--- apps/ns_server/src/ns_ports_setup.erl
+++ apps/ns_server/src/ns_ports_setup.erl
@@ -378,7 +378,8 @@ should_run(projector, Snapshot) ->
     ns_cluster_membership:should_run_service(Snapshot, kv, node()) andalso
         not config_profile:search({indexer, projector_disabled}, false);
 should_run(cont_backup, Snapshot) ->
-    ns_cluster_membership:should_run_service(Snapshot, kv, node());
+    cluster_compat_mode:is_enterprise() andalso
+        ns_cluster_membership:should_run_service(Snapshot, kv, node());
 should_run(Service, Snapshot) ->
     ns_cluster_membership:should_run_service(Snapshot, Service, node()).
 
