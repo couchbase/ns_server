626ce2c825928ec7b0a4dc0e15f138f944b7dd6a
Timofey Barmin <timofey.barmin@couchbase.com>
2025-03-12 16:10:51 -0700

    MB-65717: [BP] Don't run validate_otp_certs check if...
    ... node name is not configured yet because it will fail
    the certificate reload process with confusing reason
    
    Change-Id: I12095f60b3dcc70aaee76f9280b62a515567b482
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/225399
    Well-Formed: Restriction Checker
    Tested-by: Timofey Barmin <timofey.barmin@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Navdeep S Boparai <navdeep.boparai@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>

--- src/misc.erl
+++ src/misc.erl
@@ -371,6 +371,13 @@ extract_node_address(Node, AFamily) ->
         {_, Host} -> Host
     end.
 
+is_cb_local_nodename(Node) ->
+    LocalhostAlias = localhost_alias(),
+    case node_name_host(Node) of
+        {_, LocalhostAlias} -> true;
+        {_, _} -> false
+    end.
+
 % Get an environment variable value or return a default value
 getenv_int(VariableName, DefaultValue) ->
     case (catch list_to_integer(os:getenv(VariableName))) of
