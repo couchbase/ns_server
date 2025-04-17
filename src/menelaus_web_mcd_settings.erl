e2176dbc614907ec5019097c25c73e135d3a6e2b
Vesko Karaganev <vesko.karaganev@couchbase.com>
2025-04-17 15:37:01 +0100

    MB-66294 Add not_locked_returns_tmpfail KV setting
    The parameter is not set by default. It can be set to true to enable the
    feature via the REST API.
    
    Changes the configuration in memcached.json.
    
    Change-Id: I33d908269ecbf6b36a6fcd2ec88af343cadc85b4
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/226542
    Reviewed-by: Ben Huddleston <ben.huddleston@couchbase.com>
    Well-Formed: Restriction Checker
    Tested-by: Vesko Karaganev <vesko.karaganev@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>

--- src/menelaus_web_mcd_settings.erl
+++ src/menelaus_web_mcd_settings.erl
@@ -72,7 +72,8 @@ supported_extra_setting_names() ->
      {threads, {int, 0, ?MAX_32BIT_UNSIGNED_INT}},
      {dcp_disconnect_when_stuck_timeout_seconds,
         {int, 0, ?MAX_32BIT_UNSIGNED_INT}},
-     {dcp_disconnect_when_stuck_name_regex, string}].
+     {dcp_disconnect_when_stuck_name_regex, string},
+     {not_locked_returns_tmpfail, bool}].
 
 parse_validate_node("self") ->
     parse_validate_node(atom_to_list(node()));
