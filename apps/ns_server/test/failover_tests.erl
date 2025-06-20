2ec7558ffa857bd2fd9c630cc1d3e1afaef0d65d
Ben Huddleston <ben.huddleston@couchbase.com>
2025-06-20 17:18:54 +0100

    [BP] MB-66820: Pull failover test mocks into new module
    We can re-use much of the failover_test mock setup in future rebalance
    tests. To do so, we will move much of the mock setup to a new helper
    file and introduce a new pattern to use these helpers.
    
    The mock helpers will set up the processes or mocks that various modules
    set up. These need to be torn down later. As such, we must track these
    artifacts. To do so, we will track them in a map (generally referred to
    as the PidMap) which stores a pid, or some other marker to determine
    that this module has been started/mocked. The setup helper functions can
    be used to set up a list of modules, skipping any duplicate setup
    required. The map can be passed to the shutdown_processes/1 function to
    shut down the set up processes. Mocks can be unloaded wholly with
    meck:unload/0.
    
    I'm unsure if this an ideal longer-term solution/pattern, but it solves
    the immediately problem of mock re-use and we can consider changes as we
    use it more.
    
    Change-Id: I841682f91e055621afaaa149be969cb12508ae52
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/230907
    Tested-by: Ben Huddleston <ben.huddleston@couchbase.com>
    Reviewed-by: Peter Searby <peter.searby@couchbase.com>

--- apps/ns_server/test/failover_tests.erl
+++ apps/ns_server/test/failover_tests.erl
@@ -107,24 +107,8 @@ manual_failover_test_setup(SetupConfig) ->
     setup_node_config(maps:get(nodes, SetupConfig)),
     setup_bucket_config(maps:get(buckets, SetupConfig)),
 
-    meck:new(leader_activities),
-    meck:expect(leader_activities, run_activity,
-                fun(_Name, _Quorum, Body) ->
-                        Body()
-                end),
-    meck:expect(leader_activities, run_activity,
-                fun(_Name, _Quorum, Body, _Opts) ->
-                        Body()
-                end),
-    meck:expect(leader_activities, deactivate_quorum_nodes,
-                fun(_) -> ok end),
-
-
     meck:new(chronicle),
     meck:expect(chronicle, check_quorum, fun() -> true end),
-
-    meck:new(testconditions, [passthrough]),
-    meck:expect(testconditions, get, fun(_) -> ok end),
 
     meck:new(chronicle_master, [passthrough]),
     meck:expect(chronicle_master, deactivate_nodes,
@@ -141,84 +125,13 @@ manual_failover_test_setup(SetupConfig) ->
     %% it up and tear it down via the usual means in case the test fails.
     meck:new(chronicle_compat, [passthrough]),
 
-    %% We will spawn auto-reprovision for this test. To spawn it we must spawn
-    %% the leader_registry. Needed for leader_registry
-    meck:new(fake_ns_pubsub, [non_strict]),
-    meck:new(ns_pubsub),
-    meck:expect(ns_pubsub, subscribe_link,
-                fun(_, Handler) ->
-                        %% Stash the handler in some function, notify_key
-                        meck:expect(fake_ns_pubsub, notify_key,
-                                    fun(Key) ->
-                                            Handler(Key)
-                                    end),
-                        ok
-                end),
-
-    {ok, LeaderRegistryPid} = leader_registry:start_link(),
-    gen_server:cast(LeaderRegistryPid, {new_leader, node()}),
-
-    %% Janitor_agent mecks required to perform a full failover (with map).
-    meck:new(janitor_agent, [passthrough]),
-    meck:expect(janitor_agent, query_vbuckets,
-                fun(_,_,_,_) ->
-                        %% We don't need to return anything useful for this
-                        %% failover, we are failing over all but one node so
-                        %% we don't have to choose between any.
-                        {dict:from_list([{1, []}]), []}
-                end),
-
-    meck:expect(janitor_agent, fetch_vbucket_states,
-                fun(VBucket, _) ->
-                        %% We need to return some semi-valid vBucket stat map
-                        %% from this. We might use a couple of different maps
-                        %% for this test, so here we will generate it from
-                        %% the map (assuming only 1 vBucket).
-                        {ok, BucketConfig} = ns_bucket:get_bucket("default"),
-                        ArrayMap = array:from_list(
-                                     proplists:get_value(map, BucketConfig)),
-                        [Active | Replicas] = array:get(VBucket, ArrayMap),
-                        Seqnos = [{high_prepared_seqno, 1},
-                                  {high_seqno, 1}],
-                        A = [{Active, active, Seqnos}],
-                        R = [{Replica, replica, Seqnos} || Replica <- Replicas],
-                        A ++ R
-                end),
-
-    meck:expect(janitor_agent, find_vbucket_state,
-                fun(Node, States) ->
-                        meck:passthrough([Node, States])
-                end),
-
-    meck:expect(janitor_agent, apply_new_bucket_config,
-                fun(_,_,_,_) ->
-                        %% Just sets stuff in memcached, uninteresting here
-                        ok
-                end),
-
-    meck:expect(janitor_agent, mark_bucket_warmed,
-                fun(_,_) ->
-                        %% Just sets stuff in memcached, uninteresting here
-                        ok
-                end),
-
-    %% We need to check auto_reprovision settings via a gen_server call so we
-    %% must start up auto_reprovision. We can disable it though, because we
-    %% don't really care which options it has set.
-    fake_chronicle_kv:update_snapshot(auto_reprovision_cfg, [{enabled, false}]),
-    {ok, AutoReprovisionPid} = auto_reprovision:start_link(),
-
-    %% Return a map of pids here, we will need to shut them all down in the
-    %% teardown. Name them in case the test needs to lookup specific pids.
-    #{leader_registry => LeaderRegistryPid,
-      auto_reprovision => AutoReprovisionPid}.
+    %% Test setups return a map of pids for later shutdown in the teardown
+    mock_helpers:setup_mocks([testconditions, leader_activities,
+                              auto_reprovision,
+                              janitor_agent]).
 
 failover_test_teardown(_Config, PidMap) ->
-    maps:foreach(
-      fun(_Process, Pid) ->
-              erlang:unlink(Pid),
-              misc:terminate_and_wait(Pid, shutdown)
-      end, PidMap),
+    mock_helpers:shutdown_processes(PidMap),
 
     meck:unload(),
 
@@ -515,11 +428,6 @@ auto_failover_with_partition_test_() ->
 auto_failover_test_setup(SetupConfig) ->
     Pids = manual_failover_test_setup(SetupConfig),
 
-    %% Needed to complete a failover(/subsequent rebalance)
-    fake_ns_config:update_snapshot(rest_creds, null),
-    {ok, RebalanceReportManagerPid} = ns_rebalance_report_manager:start_link(),
-    {ok, CompatModeManagerPid} = compat_mode_manager:start_link(),
-
     %% Config for auto_failover, disabled by default. We will manually enable it
     %% or work around that in our tests. We disable it by default to avoid ticks
     %% that we may not want to handle.
@@ -530,11 +438,6 @@ auto_failover_test_setup(SetupConfig) ->
          {count, 0},
          {max_count, 5},
          {failover_preserve_durability_majority, true}]}]),
-
-    %% We need this to not throw an error in auto_failover tick, but we won't
-    %% use the status so an empty list is fine.
-    meck:new(ns_doctor),
-    meck:expect(ns_doctor, get_nodes, fun() -> [] end),
 
     meck:new(node_status_analyzer),
     meck:expect(node_status_analyzer, get_statuses,
@@ -547,41 +450,13 @@ auto_failover_test_setup(SetupConfig) ->
                           maps:get(unhealthy_nodes, SetupConfig))
                 end),
 
-    %% Needed to start the orchestrator. We don't really need the janitor to run
-    %% for this test, so we will mock it instead of run it because we'd need to
-    %% do some extra stuff to get it running.
-    meck:new(ns_janitor_server),
-    meck:expect(ns_janitor_server, start_cleanup,
-                fun(_) -> {ok, self()} end),
-    meck:expect(ns_janitor_server, terminate_cleanup,
-                fun(_) ->
-                        CallerPid = self(),
-                        CallerPid ! {cleanup_done, foo, bar},
-                        ok
-                end),
-
     %% May be required if the test tries to send an email alert (and wants to
     %% see that this has happened).
     meck:new(ns_email_alert, [passthrough]),
 
-    meck:new(cb_atomic_persistent_term, [passthrough]),
-    meck:expect(cb_atomic_persistent_term, get_or_set_if_invalid,
-                fun(_, _, F) ->
-                        F(undefined)
-                end),
-
-    %% Need to start the orchestrator so that auto_failover can follow the full
-    %% code path.
-    {ok, OrchestratorPid} = ns_orchestrator:start_link(),
-
-    %% And we must start auto_failover itself to tick it and test it as it would
-    %% normally run.
-    {ok, AutoFailoverPid} = auto_failover:start_link(),
-
-    Pids#{orchestrator => OrchestratorPid,
-          ns_rebalance_report_manager => RebalanceReportManagerPid,
-          compat_mode_manager => CompatModeManagerPid,
-          auto_failover => AutoFailoverPid}.
+    mock_helpers:setup_mocks([compat_mode_manager,
+                              ns_orchestrator,
+                              auto_failover], Pids).
 
 get_auto_failover_reported_errors(AutoFailoverPid) ->
     sets:to_list(
@@ -829,17 +704,12 @@ graceful_failover_test_setup(SetupConfig) ->
     %% made during that.
     rebalance_test_mock_setup(),
 
-    Pids.
+    mock_helpers:setup_mocks([rebalance_quirks], Pids).
 
 rebalance_test_mock_setup() ->
     fake_ns_config:update_snapshot(rebalance_out_delay_seconds, 0),
 
     meck:expect(chronicle_compat, push, fun(_) -> ok end),
-
-    %% Not worth mocking the component parts to make this work, we should not
-    %% have any quirks anyways.
-    meck:new(rebalance_quirks, [passthrough]),
-    meck:expect(rebalance_quirks, get_quirks, fun(_,_) -> [] end),
 
     meck:expect(janitor_agent, prepare_nodes_for_rebalance,
                 fun(_,_,_) ->
---
+++ apps/ns_server/test/mock_helpers.erl
@@ -0,0 +1,273 @@
+%% @author Couchbase <info@couchbase.com>
+%% @copyright 2025-Present Couchbase, Inc.
+%%
+%% Use of this software is governed by the Business Source License included in
+%% the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+%% file, in accordance with the Business Source License, use of this software
+%% will be governed by the Apache License, Version 2.0, included in the file
+%% licenses/APL2.txt.
+%%
+%% @doc
+%% This file contains a set of functions to help with starting up various
+%% ns_server processes for the sake of testing. For example, we may want to
+%% test auto-failover, or rebalance, and to do so we need to start up the
+%% orchestrator which requires it's own set of processes (or mocks). We may
+%% start processes or mock them, depending on general complexity and the needs
+%% of the test(s) calling the functions in this module.
+%%
+%% The functions in this module are intended to set up processes in a generally
+%% minimal and re-usable way, but this may not always be possible.
+%%
+%% To accomplish this in something of a re-usable way, we establish a pattern
+%% where we:
+%% 1. Export functions to set up some process
+%% 2. These functions may compose other functions to set up their requirements
+%% 3. These functions take, and return, a map of pids (accumulator style) which
+%%    is used to track the pids of the processes they create, or the mocks that
+%%    they set up. This lets us call each function multiple times and skip
+%%    setting up a process that has already been set up, simplying test setup
+%%    when we want to re-use certain functions.
+%%    In addition, the mocking can be overriden by the caller, for some given
+%%    function, if needed, by setting that key in the map of pids.
+%% 4. We can shut down any processes set up by the functions in this module by
+%%    calling `shutdown_processes/1' with the map of pids.
+%%
+%% This file is a work in progress, I'm not 100% sure that this is the best way
+%% to do this, but it exists such that we can share these mocks between tests;
+%% in particular is was added to share mocks between the failover and rebalance
+%% tests. We may wish to evaluate this approach in the future if management of
+%% these mocks becomes more complex.
+-module(mock_helpers).
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("ns_common/include/cut.hrl").
+
+%% Export everything, we need to export the functions that set up the
+%% mocks/processes so that we can call them from the test setup functions so
+%% most everything must be exported anyway.
+-compile(export_all).
+-compile(nowarn_export_all).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%% Convenience function to set up a bunch of mocks for a list of modules.
+%% Returns a map of pids (accumulator style) for the processes that are set up
+%% such that they can be shutdown later.
+-spec setup_mocks(list()) -> map().
+setup_mocks(Modules) ->
+    setup_mocks(Modules, #{}).
+
+%% Convenience function to set up a bunch of mocks for a list of modules using
+%% the provided PidMap accumulator.
+-spec setup_mocks(list(), map()) -> map().
+setup_mocks(Modules, PidMap) ->
+    functools:chain(PidMap,
+                    [setup_mock(Module, _) || Module <- Modules]).
+
+%% Shuts down all the processes that were set up by the functions in this
+%% module.
+-spec shutdown_processes(map()) -> ok.
+shutdown_processes(PidMap) ->
+    maps:foreach(
+      fun(_Process, Pid) when is_pid(Pid) ->
+              erlang:unlink(Pid),
+              misc:terminate_and_wait(Pid, shutdown);
+         (_, _) ->
+              ok
+      end, PidMap).
+
+%%%===================================================================
+%%% Helper functions
+%%%===================================================================
+
+%% Skip setting up a mock if it's already been set up. This allows a caller to
+%% override a mock if needed, or for us to compose test setup functions without
+%% having to worry about whether a mock has already been set up.
+setup_mock(Module, PidMap) ->
+    case maps:get(Module, PidMap, undefined) of
+        undefined ->
+            ?MODULE:Module(PidMap);
+        _ ->
+            PidMap
+    end.
+
+%%%===================================================================
+%%% Process setup functions
+%%%===================================================================
+
+leader_registry(PidMap) ->
+    PidMap0 = setup_mocks([ns_pubsub],
+                          PidMap),
+
+    {ok, LeaderRegistryPid} = leader_registry:start_link(),
+    gen_server:cast(LeaderRegistryPid, {new_leader, node()}),
+    PidMap0#{?FUNCTION_NAME => LeaderRegistryPid}.
+
+auto_reprovision(PidMap) ->
+    %% TODO: Nothing needs auto_reprovision to be enabled yet, just running,
+    %% so we can disable it for now.
+    fake_chronicle_kv:update_snapshot(auto_reprovision_cfg, [{enabled, false}]),
+
+    PidMap0 = setup_mocks([leader_registry],
+                          PidMap),
+
+    {ok, AutoReprovisionPid} = auto_reprovision:start_link(),
+    PidMap0#{?FUNCTION_NAME => AutoReprovisionPid}.
+
+rebalance_report_manager(PidMap) ->
+    fake_ns_config:update_snapshot(rest_creds, null),
+
+    meck:new(cb_atomic_persistent_term, [passthrough]),
+    meck:expect(cb_atomic_persistent_term, get_or_set_if_invalid,
+                fun(_, _, F) ->
+                        F(undefined)
+                end),
+    {ok, RebalanceReportManagerPid} = ns_rebalance_report_manager:start_link(),
+    PidMap#{?FUNCTION_NAME => RebalanceReportManagerPid}.
+
+ns_orchestrator(PidMap) ->
+    PidMap0 = setup_mocks([ns_janitor_server,
+                           ns_doctor,
+                           rebalance_report_manager,
+                           testconditions],
+                          PidMap),
+
+    {ok, OrchestratorPid} = ns_orchestrator:start_link(),
+    PidMap0#{?FUNCTION_NAME => OrchestratorPid}.
+
+compat_mode_manager(PidMap) ->
+    {ok, CompatModeManagerPid} = compat_mode_manager:start_link(),
+    PidMap#{?FUNCTION_NAME => CompatModeManagerPid}.
+
+auto_failover(PidMap) ->
+    PidMap0 = setup_mocks([compat_mode_events], PidMap),
+
+    {ok, AutoFailoverPid} = auto_failover:start_link(),
+    PidMap0#{?FUNCTION_NAME => AutoFailoverPid}.
+
+compat_mode_events(PidMap) ->
+    {ok, CompatModeEventsPid} = gen_event:start_link({local,
+                                                      compat_mode_events}),
+    PidMap#{?FUNCTION_NAME => CompatModeEventsPid}.
+
+%%%===================================================================
+%%% Mock setup functions
+%%%
+%%% These functions just set up mocks, so we don't have a pid to add to the map.
+%%% We just return an atom so that we can still handle skipping multiple
+%%% setups/overriding setups.
+%%%===================================================================
+
+janitor_agent(PidMap) ->
+    %% Janitor_agent mecks required to perform a full failover (with map).
+    meck:new(janitor_agent, [passthrough]),
+    meck:expect(janitor_agent, query_vbuckets,
+                fun(_,_,_,_) ->
+                        %% We don't need to return anything useful for this
+                        %% failover, we are failing over all but one node so
+                        %% we don't have to choose between any.
+                        {dict:from_list([{1, []}]), []}
+                end),
+
+    meck:expect(janitor_agent, fetch_vbucket_states,
+                fun(VBucket, _) ->
+                        %% We need to return some semi-valid vBucket stat map
+                        %% from this. We might use a couple of different maps
+                        %% for this test, so here we will generate it from
+                        %% the map (assuming only 1 vBucket).
+                        {ok, BucketConfig} = ns_bucket:get_bucket("default"),
+                        ArrayMap = array:from_list(
+                                     proplists:get_value(map, BucketConfig)),
+                        [Active | Replicas] = array:get(VBucket, ArrayMap),
+                        Seqnos = [{high_prepared_seqno, 1},
+                                  {high_seqno, 1}],
+                        A = [{Active, active, Seqnos}],
+                        R = [{Replica, replica, Seqnos} || Replica <- Replicas],
+                        A ++ R
+                end),
+
+    meck:expect(janitor_agent, find_vbucket_state,
+                fun(Node, States) ->
+                        meck:passthrough([Node, States])
+                end),
+
+    meck:expect(janitor_agent, apply_new_bucket_config,
+                fun(_,_,_,_) ->
+                        %% Just sets stuff in memcached, uninteresting here
+                        ok
+                end),
+
+    meck:expect(janitor_agent, mark_bucket_warmed,
+                fun(_,_) ->
+                        %% Just sets stuff in memcached, uninteresting here
+                        ok
+                end),
+    PidMap#{?FUNCTION_NAME => mocked}.
+
+%% auto failover for orchestrator
+ns_janitor_server(PidMap) ->
+    meck:new(ns_janitor_server),
+    meck:expect(ns_janitor_server, start_cleanup,
+                fun(_) -> {ok, self()} end),
+    meck:expect(ns_janitor_server, terminate_cleanup,
+                fun(_) ->
+                        CallerPid = self(),
+                        CallerPid ! {cleanup_done, foo, bar},
+                        ok
+                end),
+    PidMap#{?FUNCTION_NAME => mocked}.
+
+%% from manual failover setup
+leader_activities(PidMap) ->
+    meck:new(leader_activities),
+    meck:expect(leader_activities, run_activity,
+                fun(_Name, _Quorum, Body) ->
+                        Body()
+                end),
+    meck:expect(leader_activities, run_activity,
+                fun(_Name, _Quorum, Body, _Opts) ->
+                        Body()
+                end),
+    meck:expect(leader_activities, deactivate_quorum_nodes,
+            fun(_) -> ok end),
+    PidMap#{?FUNCTION_NAME => mocked}.
+
+ns_doctor(PidMap) ->
+    meck:new(ns_doctor),
+    meck:expect(ns_doctor, get_nodes, fun() -> [] end),
+
+    meck:expect(
+      ns_doctor, get_memory_data,
+      fun(Nodes) ->
+              {ok, [{Node, memsup:get_system_memory_data()} || Node <- Nodes]}
+      end),
+    PidMap#{?FUNCTION_NAME => mocked}.
+
+rebalance_quirks(PidMap) ->
+    meck:new(rebalance_quirks, [passthrough]),
+    meck:expect(rebalance_quirks, get_quirks, fun(_,_) -> [] end),
+    PidMap#{?FUNCTION_NAME => mocked}.
+
+testconditions(PidMap) ->
+    meck:new(testconditions, [passthrough]),
+    meck:expect(testconditions, get, fun(_) -> ok end),
+    PidMap#{?FUNCTION_NAME => mocked}.
+
+ns_pubsub(PidMap) ->
+    meck:new(fake_ns_pubsub, [non_strict]),
+    meck:new(ns_pubsub, [passthrough]),
+    meck:expect(ns_pubsub, subscribe_link,
+                %% We are only handling chronicle_compat_events this way,
+                %% everything else is done via the appropriate gen_event.
+                fun(_, Handler) ->
+                        %% Stash the handler in some function, notify_key
+                        meck:expect(fake_ns_pubsub, notify_key,
+                                    fun(Key) ->
+                                            Handler(Key)
+                                    end),
+                        ok
+                end),
+
+    PidMap#{?FUNCTION_NAME => mocked}.
