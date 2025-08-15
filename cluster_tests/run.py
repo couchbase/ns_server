751c96a6afc0727a92c90722d745e7188f93bb0c
Steve Watanabe <steve.watanabe@couchbase.com>
2025-08-15 10:40:50 -0700

    Remove fusion cluster tests
    Fusion has been removed from the morpheus manifest so the associated
    cluster test in ns_server is being removed.
    
    Change-Id: Ic47383f0e25abc4c2615e7e7341e1d319b34e175
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/232335
    Tested-by: Steve Watanabe <steve.watanabe@couchbase.com>
    Reviewed-by: Artem Stemkovski <artem@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>

--- cluster_tests/run.py
+++ cluster_tests/run.py
@@ -76,8 +76,7 @@ from testsets import \
     metakv2_tests, \
     app_telemetry_tests, \
     jwt_tests, \
-    bucket_dirs_cleanup_tests, \
-    fusion_tests
+    bucket_dirs_cleanup_tests
 
 tmp_cluster_dir = os.path.join(testlib.get_cluster_test_dir(),
                                "test_cluster_data")
