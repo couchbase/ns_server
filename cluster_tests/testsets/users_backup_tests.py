3bd3b365b4f71ce5425f503161d71078448bb565
Steve Watanabe <steve.watanabe@couchbase.com>
2025-08-21 15:58:01 -0700

    MB-68079: Get more info for cluster test failure
    Change-Id: I37b9730fff65c002cad00c852b9c626436b658e8
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/232667
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Steve Watanabe <steve.watanabe@couchbase.com>
    Reviewed-by: Neelima Premsankar <neelima.premsankar@couchbase.com>

--- cluster_tests/testsets/users_backup_tests.py
+++ cluster_tests/testsets/users_backup_tests.py
@@ -32,6 +32,7 @@ class UsersBackupTests(testlib.BaseTestSet):
         local_users = testlib.get_succ(self.cluster,
                                        '/settings/rbac/users/local').json()
         self.local_users_count = len(local_users)
+        self.local_users = local_users
         ext_users = testlib.get_succ(self.cluster,
                                      '/settings/rbac/users/external').json()
         self.external_users_count = len(ext_users)
@@ -53,8 +54,10 @@ class UsersBackupTests(testlib.BaseTestSet):
         assert 'version' in backup
         assert backup['version'] == '1'
         assert 'admin' in backup
-        assert len(backup['users']) == self.local_users_count + \
-                                       self.external_users_count
+        if len(backup['users']) != \
+                self.local_users_count + self.external_users_count:
+                    users = [user['id'] for user in backup.get('users', [])]
+                    assert users == None
         assert len(backup['groups']) == self.groups_count
 
 
