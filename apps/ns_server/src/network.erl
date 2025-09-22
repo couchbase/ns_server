7df86b77974ffc2f52ae6af1c89b21d2699cd595
Peter Searby <peter.searby@couchbase.com>
2025-09-22 12:13:52 +0100

    MB-68185: Don't depend on internal representation of sslsocket
    Updates all the functions using sslsocket in network.erl to keep them
    consistent.
    
    This is needed to retain compatibility in OTP 28 when the internal
    representation of the sslsocket type was changed:
    https://github.com/erlang/otp/pull/8786
    
    Change-Id: Ic64fb325774b273d2420519bf9ab51f1847395ff
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/234589
    Reviewed-by: Neelima Premsankar <neelima.premsankar@couchbase.com>
    Tested-by: Peter Searby <peter.searby@couchbase.com>

--- apps/ns_server/src/network.erl
+++ apps/ns_server/src/network.erl
@@ -38,45 +38,44 @@ socket_connect(ssl, Host, Port, Opts, Timeout) ->
 socket_connect(tcp, Host, Port, Opts, Timeout) ->
     gen_tcp:connect(Host, Port, Opts, Timeout).
 
--spec socket_setopts(ssl:sslsocket() | gen_tcp:socket(),
+-spec socket_setopts(gen_tcp:socket() | ssl:sslsocket(),
                      [gen_tcp:option()]) ->
                             ok | {error, term()}.
-socket_setopts({sslsocket, _, _} = Sock, Opts) ->
-    ssl:setopts(Sock, Opts);
+socket_setopts(Sock, Opts) when is_port(Sock)->
+    inet:setopts(Sock, Opts);
 socket_setopts(Sock, Opts) ->
-    inet:setopts(Sock, Opts).
+    ssl:setopts(Sock, Opts).
 
--spec socket_send(ssl:sslsocket() | gen_tcp:socket(),
+-spec socket_send(gen_tcp:socket() | ssl:sslsocket(),
                   term()) ->
                   ok | {error, term()}.
-socket_send({sslsocket, _, _} = Socket, Data) ->
-    ssl:send(Socket, Data);
+socket_send(Socket, Data) when is_port(Socket) ->
+    prim_inet:send(Socket, Data);
 socket_send(Socket, Data) ->
-    prim_inet:send(Socket, Data).
+    ssl:send(Socket, Data).
 
--spec socket_recv(ssl:sslsocket() | gen_tcp:socket(),
+-spec socket_recv(gen_tcp:socket() | ssl:sslsocket(),
                   integer(), timeout()) ->
                          {ok, binary() | list()} |
                          {error, term()}.
-socket_recv({sslsocket, _, _} = Socket, NumBytes, Timeout) ->
-    ssl:recv(Socket, NumBytes, Timeout);
+socket_recv(Socket, NumBytes, Timeout) when is_port(Socket) ->
+    prim_inet:recv(Socket, NumBytes, Timeout);
 socket_recv(Socket, NumBytes, Timeout) ->
-    prim_inet:recv(Socket, NumBytes, Timeout).
+    ssl:recv(Socket, NumBytes, Timeout).
 
 -spec socket_close(Socket) -> term() when
-      Socket :: ssl:sslsocket() | gen_tcp:socket().
-socket_close({sslsocket, _, _} = Socket) ->
-    ssl:close(Socket);
+      Socket :: gen_tcp:socket() | ssl:sslsocket().
+socket_close(Socket) when is_port(Socket) ->
+    gen_tcp:close(Socket);
 socket_close(Socket) ->
-    gen_tcp:close(Socket).
+    ssl:close(Socket).
 
--spec sockname({ssl, ssl:sslsocket()} | gen_tcp:socket()) ->
+-spec sockname(gen_tcp:socket() | {ssl, ssl:sslsocket()}) ->
     {ok,
         {inet:ip_address(), inet:port_number()} |
          inet:returned_non_ip_address()} |
         {error, inet:posix() | SslReason :: any()}.
-
+sockname(Socket) when is_port(Socket) ->
+    inet:sockname(Socket);
 sockname({ssl, Socket}) ->
-    ssl:sockname(Socket);
-sockname(Socket) ->
-    inet:sockname(Socket).
+    ssl:sockname(Socket).
