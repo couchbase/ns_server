df7457b6a8af7006e9f5e112ede7c633c14c928b
Jim Walker <jim@couchbase.com>
2026-02-25 14:18:42 +0000

    MB-70640: Add dcp_proxy suppresion for opcode 0x68
    This is the DCP Cache Transfer end message which can be suppressed.
    
    Change-Id: I715dded8bdb9831c0ce8ff882eed17de3e7f8274
    Reviewed-on: https://review.couchbase.org/c/ns_server/+/240758
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Ben Huddleston <ben.huddleston@couchbase.com>
    Tested-by: Jim Walker <jim@couchbase.com>

--- apps/ns_server/include/mc_constants.hrl
+++ apps/ns_server/include/mc_constants.hrl
@@ -245,6 +245,7 @@
 -define(DCP_OSO_SNAPSHOT,          16#65).
 -define(DCP_CACHED_VALUE,          16#66).
 -define(DCP_CACHED_KEY_META,       16#67).
+-define(DCP_CACHE_TRANSFER_END,    16#68).
 
 %% Snapshot commands (For file based rebalance)
 -define(PREPARE_SNAPSHOT, 16#e0).
